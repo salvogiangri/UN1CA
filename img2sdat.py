c9fdc395551cf8914910f25b043ffeea7e8c514e
xpirt <xpirt@users.noreply.github.com>
2015-01-29 16:03:12 +0100

    img2sdat.py: win32/64 compatible + clean up

--- img2sdat.py
+++ img2sdat.py
@@ -1,23 +1,33 @@
-import sys
-import blockimgdiff
-import sparse_img
+#!/usr/bin/env python
+#encoding:utf8
+#====================================================
+#          FILE: img2sdat.py
+#       AUTHORS: xpirt - luxi78 - howellzhu
+#          DATE: 2015-01-29 15:27:14 CST
+#====================================================
+import sys, blockimgdiff, sparse_img, os
 
 def main(sysimg, outdir):
-    print 'img2sdat tools from Howellzhu@gmail.com'
     tgt = sparse_img.SparseImage(sysimg)
     bif = blockimgdiff.BlockImageDiff(tgt, None)
     bif.Compute('system', outdir)
     return
 
-
 if __name__ == '__main__':
     sysimg = 'system.img'
     outdir = './'
+    if len(sys.argv) == 1:
+        print ("\nimg2sdat - usage is: \n\n      img2sdat [system.img] [outdir]\n\n")
+        print ("Visit xda thread for more information.\n")
+        os.system("pause")
+        sys.exit(1)
     if len(sys.argv) >= 2:
         sysimg = sys.argv[1]
     if len(sys.argv) >= 3:
         outdir = sys.argv[2] + '/'
     if len(sys.argv) >= 4:
-        print 'Usage: %s [system.img] [outdir]' % sys.argv[0]
+        print ("\nimg2sdat - usage is: \n\n      img2sdat [system.img] [outdir]" % sys.argv[0])
+        print ("Visit xda thread for more information.\n")
+        os.system("pause")
         sys.exit(1)
     main(sysimg, outdir)
