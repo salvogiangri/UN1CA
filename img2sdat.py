8cdd74692cc121d2a1f4f099f76502fa1b2f127e
Andrei Conache <xpirt.dev@gmail.com>
2016-11-23 16:48:28 +0100

    img2sdat.py: version 1.0 and fixes
    fixed utf-8 encoding
    fixed transfer list file not generated properly

--- img2sdat.py
+++ img2sdat.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python
-#encoding:utf8
+# -*- coding: utf-8 -*-
 #====================================================
 #          FILE: img2sdat.py
 #       AUTHORS: xpirt - luxi78 - howellzhu
-#          DATE: 2015-10-11 14:37:22 CST
+#          DATE: 2016-11-23 16:20:11 CST
 #====================================================
 
-import sys, blockimgdiff, sparse_img, os
+import sys, os, errno, tempfile
+import common, blockimgdiff, sparse_img
 
-def main(sysimg, outdir):
-    tgt = sparse_img.SparseImage(sysimg)
-    bif = blockimgdiff.BlockImageDiff(tgt, None)
-    bif.Compute(outdir)
+__version__ = '1.0'
+
+if sys.hexversion < 0x02070000:
+    print >> sys.stderr, "Python 2.7 or newer is required."
+    try:
+       input = raw_input
+    except NameError: pass
+    input('Press ENTER to exit...')
+    sys.exit(1)
+else:
+    print('img2sdat binary - version: %s\n' % __version__)
+
+try:
+    INPUT_IMAGE = str(sys.argv[1])
+except IndexError:
+    print('Usage: img2sdat.py <system_img>\n')
+    print('    <system_img>: input system image\n')
+    print("Visit xda thread for more information.\n")
+    try:
+       input = raw_input
+    except NameError: pass
+    input('Press ENTER to exit...')
+    sys.exit()
+
+def main(argv):
+    # Get sparse image
+    input_image = sparse_img.SparseImage(INPUT_IMAGE, tempfile.mkstemp()[1], '0')
+    
+    # Generate output files
+    b = blockimgdiff.BlockImageDiff(input_image, None)
+    b.Compute('system')
+    
+    print('Done! Output files: %s' % os.path.dirname(__file__))
     return
 
 if __name__ == '__main__':
-    sysimg = 'system.img'
-    outdir = './'
-    if len(sys.argv) == 1:
-        print ("\nimg2sdat - usage is: \n\n      img2sdat [system.img] [outdir]\n\n")
-        print ("Visit xda thread for more information.\n")
-        try:
-            input = raw_input
-        except NameError: pass
-        input ("Press ENTER to exit...\n")
-        sys.exit(1)
-    if len(sys.argv) >= 2:
-        sysimg = sys.argv[1]
-    if len(sys.argv) >= 3:
-        outdir = sys.argv[2] + '/'
-    if len(sys.argv) >= 4:
-        print ("\nimg2sdat - usage is: \n\n      img2sdat [system.img] [outdir]" % sys.argv[0])
-        print ("Visit xda thread for more information.\n")
-        try:
-            input = raw_input
-        except NameError: pass
-        input ("Press ENTER to exit...\n")
-        sys.exit(1)
-    main(sysimg, outdir)
+    main(sys.argv)
