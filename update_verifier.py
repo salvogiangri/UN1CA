7c4c29b56d7ddc8ad0bc8eda5cdbc2e062d2b958
Harry Youd <harry@harryyoud.co.uk>
2018-05-22 04:57:05 +0100

    Fix some pylints

--- update_verifier.py
+++ update_verifier.py
@@ -1,23 +1,26 @@
 from __future__ import print_function
 import os
+import sys
 from pyasn1.codec.der.decoder import decode as der_decoder
 import rsa
-import sys
 
 FOOTER_SIZE = 6
 EOCD_HEADER_SIZE = 22
 
-class SignedFile:
-    def __init__(self, file):
-        self.file = file
-        self.length = os.path.getsize(file)
+class SignedFile(object):
+    def __init__(self, filepath):
+        self.eocd = None
+        self.eocd_size = None
+        self.filepath = filepath
         self.footer = None
+        self.length = os.path.getsize(filepath)
         self.signed_len = None
+        self.signature_start = None
 
     def get_footer(self):
-        with open(self.file, 'rb') as zip:
-            zip.seek(-FOOTER_SIZE, os.SEEK_END)
-            self.footer = bytearray(zip.read())
+        with open(self.filepath, 'rb') as zipfile:
+            zipfile.seek(-FOOTER_SIZE, os.SEEK_END)
+            self.footer = bytearray(zipfile.read())
 
     def check_footer(self):
         if self.footer is None:
@@ -37,34 +40,33 @@ class SignedFile:
     def check_eocd(self):
         if self.footer is None:
             self.check_footer()
-        with open(self.file, 'rb') as zip:
-            zip.seek(-self.eocd_size, os.SEEK_END)
-            self.eocd = bytearray(zip.read(self.eocd_size))
+        with open(self.filepath, 'rb') as zipfile:
+            zipfile.seek(-self.eocd_size, os.SEEK_END)
+            self.eocd = bytearray(zipfile.read(self.eocd_size))
             assert self.eocd[0:4] == bytearray([80, 75, 5, 6]), (
                 "EOCD has wrong magic")
             for i in range(0, self.eocd_size-1):
-                zip.seek(-i, os.SEEK_END)
-                assert bytearray(zip.read(4)) != bytearray([80, 75, 5, 6]), (
+                zipfile.seek(-i, os.SEEK_END)
+                assert bytearray(zipfile.read(4)) != bytearray([80, 75, 5, 6]), (
                     "Multiple EOCD magics - possible exploit")
 
     def verify(self, pubkey):
         self.check_eocd()
-        with open(self.file, 'rb') as zip:
+        with open(self.filepath, 'rb') as zipfile:
             # Read everything that's been signed
-            zip.seek(0, os.SEEK_SET)
-            message = zip.read(self.signed_len)
+            zipfile.seek(0, os.SEEK_SET)
+            message = zipfile.read(self.signed_len)
             # Read the signature
-            zip.seek(-self.signature_start, os.SEEK_END)
+            zipfile.seek(-self.signature_start, os.SEEK_END)
             signature_size = self.signature_start - FOOTER_SIZE
-            signature_raw = zip.read(signature_size)
-        # pyasn1 can't decode PKCS#7, so cert contains everything and rest is
-        # empty
-        cert, rest = der_decoder(signature_raw)
+            signature_raw = zipfile.read(signature_size)
+        # pyasn1 can't decode PKCS#7, so cert (tuple[0]) contains everything
+        cert, = der_decoder(signature_raw)
         # since pyasn1 doesn't decode properly, we have to read this in a
         # stupid way. The data we need is the very last field of each entry
         signature = cert[-1][-1][-1]['field-4'].asOctets()
-        with open(pubkey) as file:
-            keydata = rsa.PublicKey.load_pkcs1(file.read())
+        with open(pubkey) as zipfile:
+            keydata = rsa.PublicKey.load_pkcs1(zipfile.read())
         try:
             return rsa.verify(message, signature, keydata)
         except rsa.pkcs1.VerificationError:
@@ -80,8 +82,8 @@ def main():
         sys.exit(2)
     zipfile = sys.argv[1]
     pubkey = sys.argv[2]
-    file = SignedFile(sys.argv[1])
-    if not file.verify(pubkey):
+    signed_file = SignedFile(zipfile)
+    if not signed_file.verify(pubkey):
         print("Failed verification", file=sys.stderr)
         sys.exit(1)
     else:
