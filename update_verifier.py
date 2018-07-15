873e371ed186f04cc4b457b8ada73b2b73f809e3
Harry Youd <harry@harryyoud.co.uk>
2018-07-15 13:34:57 +0100

    Use properties

--- update_verifier.py
+++ update_verifier.py
@@ -11,54 +11,85 @@ EOCD_HEADER_SIZE = 22
 
 class SignedFile(object):
     def __init__(self, filepath):
-        self.eocd = None
-        self.eocd_size = None
+        self._comment_size = None
+        self._eocd = None
+        self._eocd_size = None
+        self._footer = None
+        self._signed_len = None
+        self._signature_start = None
         self.filepath = filepath
-        self.footer = None
         self.length = os.path.getsize(filepath)
-        self.signed_len = None
-        self.signature_start = None
 
-    def get_footer(self):
+    @property
+    def footer(self):
+        if self._footer is not None:
+            return self._footer
         with open(self.filepath, 'rb') as zipfile:
             zipfile.seek(-FOOTER_SIZE, os.SEEK_END)
-            self.footer = bytearray(zipfile.read())
+            self._footer = bytearray(zipfile.read())
+        return self._footer
 
-    def check_footer(self):
-        if self.footer is None:
-            self.get_footer()
-        comment_size = self.footer[4] + (self.footer[5] << 8)
-        self.signature_start = self.footer[0] + (self.footer[1] << 8)
-        self.eocd_size = comment_size + EOCD_HEADER_SIZE
+    @property
+    def comment_size(self):
+        if self._comment_size is not None:
+            return self._comment_size
+        self._comment_size = self.footer[4] + (self.footer[5] << 8)
+        return self._comment_size
+
+    @property
+    def signature_start(self):
+        if self._signature_start is not None:
+            return self._signature_start
+        self._signature_start = self.footer[0] + (self.footer[1] << 8)
+        return self._signature_start
+
+    @property
+    def eocd_size(self):
+        if self._eocd_size is not None:
+            return self._eocd_size
+        self._eocd_size = self.comment_size + EOCD_HEADER_SIZE
+        return self._eocd_size
+
+    @property
+    def eocd(self):
+        if self._eocd is not None:
+            return self._eocd
+        with open(self.filepath, 'rb') as zipfile:
+            zipfile.seek(-self.eocd_size, os.SEEK_END)
+            eocd = bytearray(zipfile.read(self.eocd_size))
+        self._eocd = eocd
+        return self._eocd
+
+    @property
+    def signed_len(self):
+        if self._signed_len is not None:
+            return self._signed_len
+        signed_len = self.length - self.eocd_size + EOCD_HEADER_SIZE - 2
+        self._signed_len = signed_len
+        return self._signed_len
+
+    def check_valid(self):
         assert self.footer[2] == 255 and self.footer[3] == 255, (
             "Footer has wrong magic")
-        assert self.signature_start <= comment_size, (
+        assert self.signature_start <= self.comment_size, (
             "Signature start larger than comment")
         assert self.signature_start > FOOTER_SIZE, (
             "Signature inside footer or outside file")
         assert self.length >= self.eocd_size, "EOCD larger than length"
-        self.signed_len = self.length - self.eocd_size + EOCD_HEADER_SIZE - 2
-
-    def check_eocd(self):
-        if self.footer is None:
-            self.check_footer()
+        assert self.eocd[0:4] == bytearray([80, 75, 5, 6]), (
+            "EOCD has wrong magic")
         with open(self.filepath, 'rb') as zipfile:
-            zipfile.seek(-self.eocd_size, os.SEEK_END)
-            self.eocd = bytearray(zipfile.read(self.eocd_size))
-            assert self.eocd[0:4] == bytearray([80, 75, 5, 6]), (
-                "EOCD has wrong magic")
             for i in range(0, self.eocd_size-1):
                 zipfile.seek(-i, os.SEEK_END)
                 assert bytearray(zipfile.read(4)) != bytearray(
                     [80, 75, 5, 6]), ("Multiple EOCD magics; possible exploit")
+        return True
 
     def verify(self, pubkey):
-        self.check_eocd()
+        self.check_valid()
         with open(self.filepath, 'rb') as zipfile:
-            # Read everything that's been signed
             zipfile.seek(0, os.SEEK_SET)
             message = zipfile.read(self.signed_len)
-            # Read the signature
             zipfile.seek(-self.signature_start, os.SEEK_END)
             signature_size = self.signature_start - FOOTER_SIZE
             signature_raw = zipfile.read(signature_size)
