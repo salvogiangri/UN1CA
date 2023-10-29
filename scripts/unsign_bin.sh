84a8d9506d2348faac7359fc76d70b4435af02f3
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-10-29 14:55:03 +0100

    scripts/unsign_bin.sh: init
    Change-Id: Ic35b9c499434c6c3a08993102b1921a6b420bd12

---
+++ scripts/unsign_bin.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e
+
+# [
+SRC_DIR="$(git rev-parse --show-toplevel)"
+OUT_DIR="$SRC_DIR/out"
+TOOLS_DIR="$OUT_DIR/tools/bin"
+
+PATH="$TOOLS_DIR:$PATH"
+# ]
+
+[ -f "$OUT_DIR/config.sh" ] || exit 1
+source "$OUT_DIR/config.sh"
+
+if [ -z "$1" ]; then
+    echo "Usage: unsign_bin <image>"
+elif [ ! -f "$1" ]; then
+    echo "File not found: $1"
+else
+    if avbtool info_image --image "$1" &>/dev/null; then
+        echo "Removing AVB footer"
+        avbtool erase_footer --image "$1"
+    fi
+    if grep -q "SignerVer02" "$1"; then
+        echo "Removing Samsung v2 signature"
+        truncate -s -512 "$1"
+    fi
+    if grep -q "SignerVer03" "$1"; then
+        echo "Removing Samsung v3 signature"
+        truncate -s -784 "$1"
+    fi
+fi
+
+exit 0
