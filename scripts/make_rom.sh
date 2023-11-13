42eb717b67a15bc317fc9e1454982d24155fb3a5
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-13 21:34:02 +0100

    scripts/make_rom.sh: init
    Change-Id: I73bdfa6a7b1478eff26b8f2b1522d0395c9126fe

---
+++ scripts/make_rom.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e
+
+# [
+SRC_DIR="$(git rev-parse --show-toplevel)"
+# ]
+
+bash "$SRC_DIR/scripts/download_fw.sh"
+bash "$SRC_DIR/scripts/extract_fw.sh"
+bash "$SRC_DIR/scripts/internal/create_work_dir.sh"
+
+exit 0
