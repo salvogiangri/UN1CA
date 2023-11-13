8eb6da1ee1e844460acbf4a63d5aeedb896ce224
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-13 16:40:33 +0100

    scripts/cleanup.sh: init
    Change-Id: I5690da35e39cb5e3a0ce9f6c1f27e1aa34ea7bd0

---
+++ scripts/cleanup.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# [
+SRC_DIR="$(git rev-parse --show-toplevel)"
+OUT_DIR="$SRC_DIR/out"
+ODIN_DIR="$OUT_DIR/odin"
+FW_DIR="$OUT_DIR/fw"
+TOOLS_DIR="$OUT_DIR/tools/bin"
+# ]
+
+ALL=false
+ODIN=false
+FW=false
+TOOLS=false
+
+if [ -z "$1" ]; then
+    echo "Usage: cleanup <type>"
+    exit 1
+else
+    case "$1" in
+        "all")
+            ALL=true
+            ;;
+        *"odin"*)
+            ODIN=true
+            ;;
+        *"fw"*)
+            FW=true
+            ;;
+        *"tools"*)
+            TOOLS=true
+            ;;
+        *)
+            echo "\"$1\" is not valid type."
+            echo "Available options (multiple can be accepted with a separator):"
+            echo "all"
+            echo "odin"
+            echo "fw"
+            echo "tools"
+            exit 1
+            ;;
+    esac
+fi
+
+if $ALL; then
+    echo "- Cleaning everything..."
+    rm -rf "$OUT_DIR"
+    exit 0
+fi
+
+if $ODIN; then
+    echo "- Cleaning Odin firmwares dir..."
+    rm -rf "$ODIN_DIR"
+fi
+
+if $FW; then
+    echo "- Cleaning extracted firmwares dir..."
+    rm -rf "$FW_DIR"
+fi
+
+if $TOOLS; then
+    echo "- Cleaning dependencies dir..."
+    rm -rf "$TOOLS_DIR"
+    {
+        cd "$SRC_DIR/external/android-tools" && git clean -f -d -x && cd -
+        cd "$SRC_DIR/external/apktool" && git clean -f -d -x && cd -
+        cd "$SRC_DIR/external/erofs-utils" && git clean -f -d -x && cd -
+        cd "$SRC_DIR/external/samfirm.js" && git clean -f -d -x && cd -
+        cd "$SRC_DIR/external/smali" && git clean -f -d -x && cd -
+    } 2>&1 > /dev/null
+fi
+
+exit 0
