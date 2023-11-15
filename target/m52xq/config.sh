2cd2e305a4c84d6b8e7320cd255ba37929a3e4c6
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-15 11:32:18 +0100

    m52xq: init
    Change-Id: I0d528b7ff6e0cf343c74fd831ba8cb2195a13605

---
+++ target/m52xq/config.sh
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2023 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy M52 5G (m52xq)
+TARGET_CODENAME="m52xq"
+TARGET_FIRMWARES=( "SM-M526B/INS" )
+TARGET_SINGLE_SYSTEM_IMAGE="qssi"
+TARGET_HAS_SYSTEM_EXT=false
