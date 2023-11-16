fe2378bf21bea7d86badb3b9060d6168467aa19c
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-16 12:22:27 +0100

    scripts/make_rom.sh: add target work_dir support
    Change-Id: Id931733713aa0acc7127769629f502aac5088536

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -20,6 +20,10 @@ set -e
 
 # [
 SRC_DIR="$(git rev-parse --show-toplevel)"
+OUT_DIR="$SRC_DIR/out"
+WORK_DIR="$OUT_DIR/work_dir"
+
+source "$OUT_DIR/config.sh"
 # ]
 
 bash "$SRC_DIR/scripts/download_fw.sh"
@@ -27,6 +31,7 @@ bash "$SRC_DIR/scripts/extract_fw.sh"
 
 echo -e "- Creating work dir...\n"
 bash "$SRC_DIR/scripts/internal/create_work_dir.sh"
+bash "$SRC_DIR/target/$TARGET_CODENAME/work_dir.sh"
 
 echo -e "- Applying debloat list..."
 bash "$SRC_DIR/scripts/internal/apply_debloat.sh"
