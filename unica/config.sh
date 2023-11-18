a09708977722909ddbcccd0012b9141c1877fa32
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-18 11:34:10 +0100

    unica/config.sh: additional flags, essi support
    Change-Id: I076ed9b7acc5f0088621f318204a1d7b2241c537

--- unica/config.sh
+++ unica/config.sh
@@ -15,15 +15,26 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Current API level
-API_LEVEL=34
+# Source ROM firmware
+case "$TARGET_SINGLE_SYSTEM_IMAGE" in
+    # Qualcomm
+    "qssi")
+        # Galaxy S23 (One UI 6.0)
+        SOURCE_FIRMWARE="SM-S911B/BTE"
+        SOURCE_API_LEVEL=34
+        SOURCE_VNDK_VERSION=33
+        SOURCE_HAS_SYSTEM_EXT=true
+        ;;
+    "essi")
+        # Galaxy S23 FE (One UI 5.1)
+        SOURCE_FIRMWARE="SM-S711B/SEK"
+        SOURCE_API_LEVEL=33
+        SOURCE_HAS_SYSTEM_EXT=true
+        ;;
+    *)
+        echo "\"$TARGET_SINGLE_SYSTEM_IMAGE\" is not a valid system image."
+        return 1
+        ;;
+esac
 
-# Base ROM firmware
-# Qualcomm: Galaxy S23
-if [[ "$TARGET_SINGLE_SYSTEM_IMAGE" == "qssi" ]]; then
-    SOURCE_FIRMWARE="SM-S911B/BTE"
-    SOURCE_HAS_SYSTEM_EXT=true
-else
-    echo "\"$TARGET_SINGLE_SYSTEM_IMAGE\" is not a valid system image."
-    exit 1
-fi
+return 0
