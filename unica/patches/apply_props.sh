0b46859335dd1adbf67bc863e78d5b73c355192c
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-21 23:06:05 +0100

    unica: patches: apply_props: add file info header
    Change-Id: If74b837f1ea3e795e4e5fda10bb822f6e609a9cc

--- unica/patches/apply_props.sh
+++ unica/patches/apply_props.sh
@@ -1,20 +1,10 @@
-#!/bin/bash
-#
-# Copyright (C) 2023 BlackMesa123
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#====================================================
+# FILE:         apply_props.sh
+# AUTHOR:       BlackMesa123, xpirt
+# DESCRIPTION:  Read and applies all the ".prop"
+#               files inside the "props" folder in
+#               work_dir.
+#====================================================
 
 # shellcheck disable=SC1091
 
