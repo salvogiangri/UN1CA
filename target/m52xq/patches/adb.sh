6e0b58c85186edad338f69ab872a6e7a7c2d4419
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-21 23:33:15 +0100

    m52xq: patches: Fix ADB on boot
     persist.vendor.radio.port_index ends up being set, which causes the usb
     script to set sys.usb.config to none, breaking adb until boot-complete.
    
    Co-authored-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I47cb067c7ca6b9e04c29b7ebe3b31003092c791f

---
+++ target/m52xq/patches/adb.sh
@@ -0,0 +1,29 @@
+#====================================================
+# FILE:         adb.sh
+# AUTHOR:       BlackMesa123, AgentFabulous
+# DESCRIPTION:  Enabled ADB at boot
+#====================================================
+
+# shellcheck disable=SC1091
+
+set -e
+
+# [
+SRC_DIR="$(git rev-parse --show-toplevel)"
+OUT_DIR="$SRC_DIR/out"
+WORK_DIR="$OUT_DIR/work_dir"
+
+source "$OUT_DIR/config.sh"
+# ]
+
+sed -i \
+    "$(sed -n "/persist.sys.usb.config/=" "$WORK_DIR/vendor/default.prop") cpersist.sys.usb.config=mtp,adb" \
+    "$WORK_DIR/vendor/default.prop"
+
+if ! grep -q "persist.vendor.radio.port_index" "$WORK_DIR/vendor/etc/init/hw/init.target.rc"; then
+    {
+        echo ""
+        echo "on property:persist.vendor.radio.port_index=\"\""
+        echo "    setprop sys.usb.config adb"
+    } >> "$WORK_DIR/vendor/etc/init/hw/init.target.rc"
+fi
