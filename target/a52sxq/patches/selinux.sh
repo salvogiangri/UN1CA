41309eb9c9501a463039cb8cbea438bd8d15b82e
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-22 18:03:29 +0100

    a52sxq/m52xq: patches: selinux: add more entries
    Change-Id: I1d6023a48a011d68e2358bb73bcdf69a0404bc7b

--- target/a52sxq/patches/selinux.sh
+++ target/a52sxq/patches/selinux.sh
@@ -16,5 +16,8 @@ WORK_DIR="$OUT_DIR/work_dir"
 
 echo "Applying SELinux patches"
 sed -i "/fabriccrypto/d" "$WORK_DIR/system/system/system_ext/etc/selinux/mapping/30.0.cil"
+sed -i "/audiomirroring/d" "$WORK_DIR/system/system/system_ext/etc/selinux/mapping/30.0.cil"
+sed -i "/hal_dsms_service/d" "$WORK_DIR/system/system/system_ext/etc/selinux/mapping/30.0.cil"
+sed -i "/uwb_regulation_skip_prop/d" "$WORK_DIR/system/system/system_ext/etc/selinux/mapping/30.0.cil"
 
 exit 0
