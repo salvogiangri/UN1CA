4e26e10f2be47fae05139f38ab508bdeb5c52d9d
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-26 19:28:25 +0100

    a52sxq/m52xq: patches: miscs: adjust /product/etc/build.prop
    Change-Id: I4be35bae40d34cce739a2b03df46180df424f969

--- target/m52xq/patches/miscs.sh
+++ target/m52xq/patches/miscs.sh
@@ -32,4 +32,10 @@ if [ ! -d "$WORK_DIR/system/spu" ]; then
     echo "spu 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
 fi
 
+echo "Fix up /product/etc/build.prop"
+sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
+
 exit 0
