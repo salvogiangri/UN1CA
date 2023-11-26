b6e14861e5de992e5aa49bd56addc3787a584ccd
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-26 19:02:48 +0100

    a52sxq/m52xq: patches: miscs: minor adjustments
    Change-Id: Ieff0251e77db4f5bb501bc6566a9828238876aae

--- target/m52xq/patches/miscs.sh
+++ target/m52xq/patches/miscs.sh
@@ -18,15 +18,15 @@ source "$OUT_DIR/config.sh"
 MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
 REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
+echo "Add stock /odm/ueventd.rc"
 if [ ! -f "$WORK_DIR/odm/ueventd.rc" ]; then
-    echo "Add stock /odm/ueventd.rc"
     cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/odm/ueventd.rc" "$WORK_DIR/odm/ueventd.rc"
     echo "/odm/ueventd\.rc u:object_r:vendor_file:s0" >> "$WORK_DIR/configs/file_context-odm"
     echo "odm/ueventd.rc 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-odm"
 fi
 
+echo "Add /spu in rootfs"
 if [ ! -d "$WORK_DIR/system/spu" ]; then
-    echo "Add /spu in rootfs"
     mkdir -p "$WORK_DIR/system/spu"
     echo "/spu u:object_r:spu_file:s0" >> "$WORK_DIR/configs/file_context-system"
     echo "spu 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
