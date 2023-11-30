a60c97c066900a2c26b0dc176e777df40d13ab30
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-30 15:17:37 +0100

    unica: packages: preload: add ability to download remote apks
    Change-Id: I7069ed7f964ef4097658f255259910af23feb7c6

--- unica/packages/preload.sh
+++ unica/packages/preload.sh
@@ -12,8 +12,29 @@ set -e
 SRC_DIR="$(git rev-parse --show-toplevel)"
 OUT_DIR="$SRC_DIR/out"
 WORK_DIR="$OUT_DIR/work_dir"
+
+DOWNLOAD_APK()
+{
+    local URL="$1"
+    local APK_PATH="system/preload/$2"
+
+    echo "Downloading /$APK_PATH"
+    mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
+    curl -L -s -o "$WORK_DIR/system/$APK_PATH" -z "$WORK_DIR/system/$APK_PATH" "$URL"
+}
 # ]
 
+sed -i "/system\/preload/d" "$WORK_DIR/configs/fs_config-system" \
+    && sed -i "/system\/preload/d" "$WORK_DIR/configs/file_context-system"
+while read -r i; do
+    FILE="$(echo -n "$i"| sed "s.$WORK_DIR/system/..")"
+    [ -d "$i" ] && echo "$FILE 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    [ -f "$i" ] && echo "$FILE 0 0 644 capabilities=0x0" >> "$WORK_DIR/configs/fs_config-system"
+    FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
+    echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
+done <<< "$(find "$WORK_DIR/system/system/preload")"
+
+echo "Regenerating /system/etc/vpl_apks_count_list.txt"
 rm -f "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"
 while read -r i; do
     FILE="$(echo "$i" | sed "s.$WORK_DIR/system..")"
