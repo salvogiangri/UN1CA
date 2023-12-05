d5d1e45f9ef8e649bf5b572a266e437bbeccc281
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-05 23:45:40 +0100

    a52sxq: patches: crypto: remove
    Change-Id: Id5837c01c374d8cb59fd517b6ea39327a6568dab

--- target/a52sxq/patches/crypto/customize.sh
+++
@@ -1,8 +0,0 @@
-SKIPUNZIP=1
-
-LINE=$(sed -n "/^\/dev\/block\/bootdevice\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.qcom")
-sed -i -e "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized//g" \
-    -e "${LINE}s/,keydirectory=\/metadata\/vold\/metadata_encryption//g" \
-    -e "${LINE}s/,inlinecrypt//g" \
-    -e "${LINE}s/,wrappedkey//g" \
-    -e "${LINE}s/,,/,/g" "$WORK_DIR/vendor/etc/fstab.qcom"
