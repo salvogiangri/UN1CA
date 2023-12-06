e3f178fbd0fbeff60b9fc47864f17d9c56a6cc63
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-06 20:41:56 +0100

    a52sxq/m52xq: patches: miscs: AIDL/HIDL fix
    Change-Id: Ia0351057eff896b75b8153d89fdad3ac3002ca9f

--- target/a52sxq/patches/miscs/customize.sh
+++ target/a52sxq/patches/miscs/customize.sh
@@ -15,3 +15,23 @@ sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
+
+echo "Add stock vintf manifest"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/etc/vintf/compatibility_matrix.device.xml" \
+    "$WORK_DIR/system/system/etc/vintf/compatibility_matrix.device.xml"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/etc/vintf/manifest.xml" \
+    "$WORK_DIR/system/system/etc/vintf/manifest.xml"
+
+echo "Add HIDL face biometrics libs"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/system_ext/lib/vendor.samsung.hardware.biometrics.face@3.0.so" \
+    "$WORK_DIR/system/system/system_ext/lib"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/system_ext/lib64/vendor.samsung.hardware.biometrics.face@3.0.so" \
+    "$WORK_DIR/system/system/system_ext/lib64"
+{
+    echo "/system/system_ext/lib/vendor\.samsung\.hardware\.biometrics\.face@3\.0\.so u:object_r:system_lib_file:s0"
+    echo "/system/system_ext/lib64/vendor\.samsung\.hardware\.biometrics\.face@3\.0\.so u:object_r:system_lib_file:s0"
+} >> "$WORK_DIR/configs/file_context-system"
+{
+    echo "system/system_ext/lib/vendor.samsung.hardware.biometrics.face@3.0.so 0 0 644 capabilities=0x0"
+    echo "system/system_ext/lib64/vendor.samsung.hardware.biometrics.face@3.0.so 0 0 644 capabilities=0x0"
+} >> "$WORK_DIR/configs/fs_config-system"
--- target/m52xq/patches/miscs/customize.sh
+++ target/m52xq/patches/miscs/customize.sh
@@ -1,5 +1,3 @@
-SKIPUNZIP=1
-
 MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
 REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
@@ -15,3 +13,25 @@ sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
+
+echo "Add HIDL fingerprint biometrics libs"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/lib/android.hardware.biometrics.fingerprint@2.1.so" \
+    "$WORK_DIR/system/system/lib"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/lib/vendor.samsung.hardware.biometrics.fingerprint@3.0.so" \
+    "$WORK_DIR/system/system/lib"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/lib64/android.hardware.biometrics.fingerprint@2.1.so" \
+    "$WORK_DIR/system/system/lib64"
+cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/lib64/vendor.samsung.hardware.biometrics.fingerprint@3.0.so" \
+    "$WORK_DIR/system/system/lib64"
+{
+    echo "/system/lib/android\.hardware\.biometrics\.fingerprint@2\.1\.so u:object_r:system_lib_file:s0"
+    echo "/system/lib/vendor\.samsung\.hardware\.biometrics\.fingerprint@3\.0\.so u:object_r:system_lib_file:s0"
+    echo "/system/lib64/android\.hardware\.biometrics\.fingerprint@2\.1\.so u:object_r:system_lib_file:s0"
+    echo "/system/lib64/vendor\.samsung\.hardware\.biometrics\.fingerprint@3\.0\.so u:object_r:system_lib_file:s0"
+} >> "$WORK_DIR/configs/file_context-system"
+{
+    echo "system/lib/android.hardware.biometrics.fingerprint@2.1.so 0 0 644 capabilities=0x0"
+    echo "system/lib/vendor.samsung.hardware.biometrics.fingerprint@3.0.so 0 0 644 capabilities=0x0"
+    echo "system/lib64/android.hardware.biometrics.fingerprint@2.1.so 0 0 644 capabilities=0x0"
+    echo "system/lib64/vendor.samsung.hardware.biometrics.fingerprint@3.0.so 0 0 644 capabilities=0x0"
+} >> "$WORK_DIR/configs/fs_config-system"
