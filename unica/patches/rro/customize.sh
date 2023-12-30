771c403179d9082100de2edccad8907501e71bce
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-30 01:19:20 +0100

    unica: patches: rro: fix up
    Change-Id: Iafa86448b39d8bb40a4e3c90785310e3998a9e45

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -4,8 +4,8 @@ if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
     bash -e "$SRC_DIR/scripts/apktool.sh" d -f "/product/overlay/framework-res__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
-    rm -f "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res/values/public.xml"
+    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"
     cp -a --preserve=all \
-        "$SRC_DIR/target/$TARGET_CODENAME/overlay/"* \
+        "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
         "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"
 fi
