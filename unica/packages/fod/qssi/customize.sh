e8496f44bafbc6ec146c7bb00783a833f116fff6
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-30 15:02:10 +0100

    unica: packages: fod: fix up
    Change-Id: I338052505eb28127e3e2d6e1c1df261036ff6c72

--- unica/packages/fod/qssi/customize.sh
+++ unica/packages/fod/qssi/customize.sh
@@ -2,7 +2,7 @@ SKIPUNZIP=1
 
 if ! $SOURCE_HAS_OPTICAL_FP_SENSOR; then
     if $TARGET_HAS_OPTICAL_FP_SENSOR; then
-        cp -a --preserve=all "$SRC_DIR/unica/packages/fod/essi/system/"* "$WORK_DIR/system/system"
+        cp -a --preserve=all "$SRC_DIR/unica/packages/fod/qssi/system/"* "$WORK_DIR/system/system"
     else
         echo "TARGET_HAS_OPTICAL_FP_SENSOR is not set. Ignoring"
     fi
