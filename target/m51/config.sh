b4139970d1063d29c41c469d63b5cb1d2e15b459
iambinaytiwari <binaytiwari6@gmail.com>
2024-01-08 18:35:04 +0530

    m51: init
    Change-Id: Idd526d72814989a3d50dd49c20dcf96010487c0c

---
+++ target/m51/config.sh
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy M51 (m51)
+TARGET_NAME="Galaxy M51"
+TARGET_CODENAME="M51"
+TARGET_FIRMWARE="SM-M515F/INS"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_API_LEVEL=32
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="qssi"
+TARGET_OS_FILE_SYSTEM="ext4"
+TARGET_INCLUDE_MAGISK=true
+TARGET_SUPER_PARTITION_SIZE=8053063680
+TARGET_SUPER_GROUP_SIZE=8048869376
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_HAS_KNOX_DUALDAR=false
+TARGET_HAS_KNOX_SDP=true
+TARGET_HAS_MASS_CAMERA_APP=true
+TARGET_HAS_OPTICAL_FP_SENSOR=false
+TARGET_IS_ESIM_SUPPORTED=false
