a700161b7df1d585555166ac1dd84e2160e0fa1e
BlackMesa123 <giangrecosalvo9@gmail.com>
2024-01-08 23:39:33 +0100

    unica: patches: stock_blobs: add stock alsa.conf
    Change-Id: Id55adbf277c16484a0e49ad06f681886b985b642

--- unica/patches/stock_blobs/customize.sh
+++ unica/patches/stock_blobs/customize.sh
@@ -88,3 +88,23 @@ while read -r i; do
     FILE="$(echo -n "$FILE" | sed 's/\./\\./g')"
     echo "/$FILE u:object_r:system_file:s0" >> "$WORK_DIR/configs/file_context-system"
 done <<< "$(find "$WORK_DIR/system/system/cameradata")"
+
+if [ -f "$FW_DIR/${MODEL}_${REGION}/system/system/usr/share/alsa/alsa.conf" ]; then
+    echo "Add stock alsa.conf"
+    cp -a --preserve=all "$FW_DIR/${MODEL}_${REGION}/system/system/usr/share/alsa/alsa.conf" \
+        "$WORK_DIR/system/system/usr/share/alsa/alsa.conf"
+    if ! grep -q "alsa\.conf" "$WORK_DIR/configs/file_context-system"; then
+        {
+            echo "/system/usr/share/alsa u:object_r:system_file:s0"
+            echo "/system/usr/share/alsa/alsa\.conf u:object_r:system_file:s0"
+        } >> "$WORK_DIR/configs/file_context-system"
+    fi
+    if ! grep -q "alsa.conf" "$WORK_DIR/configs/fs_config-system"; then
+        {
+            echo "system/usr/share/alsa 0 0 755 capabilities=0x0"
+            echo "system/usr/share/alsa/alsa.conf 0 0 644 capabilities=0x0"
+        } >> "$WORK_DIR/configs/fs_config-system"
+    fi
+else
+    REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/usr/share/alsa"
+fi
