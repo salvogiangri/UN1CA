dda0ae7476fd39240d26820d935d36dc2497b527
BlackMesa123 <giangrecosalvo9@gmail.com>
2024-01-11 11:21:55 +0100

    unica: move boot_sound mod in mods
    Change-Id: Ia334bf1e8f16bb6507edfc61b27c64e8b56bd635

--- unica/patches/mods/customize.sh
+++ unica/patches/mods/customize.sh
@@ -1,3 +1,16 @@
+if ! grep -q "system/media/audio/ui/Power" "$WORK_DIR/configs/file_context-system"; then
+    {
+        echo "/system/media/audio/ui/PowerOn\.ogg u:object_r:system_file:s0"
+        echo "/system/media/audio/ui/PowerOff\.ogg u:object_r:system_file:s0"
+    } >> "$WORK_DIR/configs/file_context-system"
+fi
+if ! grep -q "system/media/audio/ui/Power" "$WORK_DIR/configs/fs_config-system"; then
+    {
+        echo "system/media/audio/ui/PowerOn.ogg 0 0 644 capabilities=0x0"
+        echo "system/media/audio/ui/PowerOff.ogg 0 0 644 capabilities=0x0"
+    } >> "$WORK_DIR/configs/fs_config-system"
+fi
+
 echo "ro.unica.version=$ROM_VERSION" >> "$WORK_DIR/system/system/build.prop"
 
 if [ ! -d "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk" ]; then
