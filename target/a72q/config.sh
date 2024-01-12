03913133cc7f0e085085317a5c8d6824da63c945
Ruchit Marathe <risenid@duck.com>
2024-01-12 19:22:34 +0530

    Add Galaxy A72 (a72q) support (#55)
    * Add Galaxy A72 (a72q) support
    
    Import a52q configs and modify for a72q
    
    Co-Authored-By: ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
    Co-Authored-By: Salvo Giangreco <13062958+BlackMesa123@users.noreply.github.com>
    
    * a72q: overlay: fix up
    
    Change-Id: I49318c1c508e2b3b826dc3f859779f189f73bbaa
    
    * a72q: patches: camera: do not remove ApexService
    
    Change-Id: I3489d93f264dab3f7ae29a571c8ac6178d9df01b
    
    * a72q: patches: miscs: minor adjustments
    
    Change-Id: I9b1ef92668805f9c9d469baa57df194bad93189b
    
    * a72q: config: minor adjustments
    
    Change-Id: I072ab19700644da84b2e259897ed67ededfcb500
    
    * a72q: floating_feature: minor adjustments
    
    Change-Id: I63a493f43fe66d9fde87fc37e72054cdc0db6005
    
    * a72q: patches: spf: fix up
    
    Change-Id: Ie53b0051ba9bac279b4406518cd6abd4594d97f7
    
    * docs: devices: a72q: init
    
    Change-Id: Ie86c68f1cd6fe7c69a9637efc0de247861dbf5cd
    
    * a72q: overlay: add rounded corners value
    
    Change-Id: I7be8892a14412a27bc5a1d25c8b73ec995267933
    
    * a72q: link to oui 6 fw
    
    * a52q: link to oui 6 fw
    
    * a72q: overlay: add power button location
    
    Change-Id: I20ece811d37c248600607fab88cfd1cb175bebbb
    
    * a72q: patches: spf: minor adjustments
    
    Change-Id: I2608b2a4dbbd05f270e5686a5026e71da5bbca0c
    
    * a72q: patches: stock_blobs: add missing feature xml, sensorhub config
    
    Change-Id: Ia578f2201805075ff0bc63c01bcbd331678c694a
    
    ---------
    
    Co-authored-by: ShaDisNX255 <38537144+ShaDisNX255@users.noreply.github.com>
    Co-authored-by: Salvo Giangreco <13062958+BlackMesa123@users.noreply.github.com>
    Co-authored-by: BlackMesa123 <giangrecosalvo9@gmail.com>

---
+++ target/a72q/config.sh
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A72 (a72q)
+TARGET_NAME="Galaxy A72"
+TARGET_CODENAME="a72q"
+TARGET_FIRMWARE="SM-A725F/SER/353546291234560"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_API_LEVEL=34
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="qssi"
+TARGET_OS_FILE_SYSTEM="ext4"
+TARGET_SUPER_PARTITION_SIZE=10292822016
+TARGET_SUPER_GROUP_SIZE=10288627712
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_HAS_KNOX_DUALDAR=false
+TARGET_HAS_KNOX_SDP=true
+TARGET_HAS_MASS_CAMERA_APP=true
+TARGET_HAS_OPTICAL_FP_SENSOR=true
+TARGET_IS_ESIM_SUPPORTED=false
