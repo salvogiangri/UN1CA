288feddb8065673a83dc96d9d34a8e9e819014f5
Ruchit <risenid@duck.com>
2024-01-29 22:53:51 +0530

    sm7125: Ascendia, ascend!
    Self promotion bess lol

---
+++ target/a72q/patches/ascendia/customize.sh
@@ -0,0 +1,22 @@
+SKIPUNZIP=1
+
+# [
+ASCENDIA_IMG="https://downloads.simon1511.de/s/nN65wd7AdtBg95A/download/Ascendia_1.0_Vanilla_OneUI_a72q_boot.img"
+
+REPLACE_KERNEL_BINARIES()
+{
+    [ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
+    mkdir -p "$TMP_DIR"
+
+    echo "Downloading $(basename "$ASCENDIA_IMG")"
+    curl -L -s -o "$TMP_DIR/boot.img" "$ASCENDIA_IMG"
+
+    echo "Extracting kernel binaries"
+    rm -f "$WORK_DIR/kernel/boot.img"
+    cp "$TMP_DIR/boot.img" "$WORK_DIR/kernel/"
+
+    rm -rf "$TMP_DIR"
+}
+# ]
+
+REPLACE_KERNEL_BINARIES
