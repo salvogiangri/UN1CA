a3b402f1c1c938578da41993de1aa7290f6f6f94
BlackMesa123 <giangrecosalvo9@gmail.com>
2024-05-30 12:22:20 +0200

    unica: mods: burp
    Change-Id: Ibfa71186424fff8076af68730e3a4e67f4c832b9

--- unica/mods/callsounds/customize.sh
+++ unica/mods/callsounds/customize.sh
@@ -1,4 +1,4 @@
 if [ ! -d "$APKTOOL_DIR/system/priv-app/Telecom/Telecom.apk" ]; then
     bash "$SRC_DIR/scripts/apktool.sh" d "/system/priv-app/Telecom/Telecom.apk"
 fi
-cp -a --preserve=all "$SRC_DIR/unica/mods/mods/Telecom.apk/"* "$APKTOOL_DIR/system/priv-app/Telecom/Telecom.apk"
+cp -a --preserve=all "$SRC_DIR/unica/mods/callsounds/Telecom.apk/"* "$APKTOOL_DIR/system/priv-app/Telecom/Telecom.apk"
--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -4,4 +4,4 @@ echo "ro.unica.codename=$ROM_CODENAME" >> "$WORK_DIR/system/system/build.prop"
 if [ ! -d "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk" ]; then
     bash "$SRC_DIR/scripts/apktool.sh" d "/system/priv-app/SecSettings/SecSettings.apk"
 fi
-cp -a --preserve=all "$SRC_DIR/unica/mods/mods/SecSettings.apk/"* "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk"
+cp -a --preserve=all "$SRC_DIR/unica/mods/settings/SecSettings.apk/"* "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk"
