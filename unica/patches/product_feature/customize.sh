f87c37432b0580263069d7a2396e553caf3aa9d7
BlackMesa123 <giangrecosalvo9@gmail.com>
2024-06-01 15:35:23 +0200

    unica: patches: product_feature: add mDNIe patches
    Change-Id: I98d64233a1745bd5a7f0aca92c15fec615573b8a

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -19,6 +19,8 @@ GET_FP_SENSOR_TYPE()
 # ]
 
 if [[ "$(GET_FP_SENSOR_TYPE "$SOURCE_FP_SENSOR_CONFIG")" != "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" ]]; then
+    echo "Applying fingerprint sensor patches"
+
     DECOMPILE "system/framework/framework.jar"
     DECOMPILE "system/framework/services.jar"
     DECOMPILE "system/priv-app/SecSettings/SecSettings.apk"
@@ -38,5 +40,33 @@ if [[ "$(GET_FP_SENSOR_TYPE "$SOURCE_FP_SENSOR_CONFIG")" != "$(GET_FP_SENSOR_TYP
     # TODO: handle ultrasonic & capacitive fp devices
     if [[ "$(GET_FP_SENSOR_TYPE "$TARGET_FP_SENSOR_CONFIG")" == "optical" ]]; then
         cp -a --preserve=all "$SRC_DIR/unica/patches/product_feature/optical_fod/system/"* "$WORK_DIR/system/system"
+    fi
+fi
+
+if $SOURCE_HAS_HW_MDNIE; then
+    if ! $TARGET_HAS_HW_MDNIE; then
+        echo "Applying mDNIe patches"
+
+        DECOMPILE "system/framework/framework.jar"
+        DECOMPILE "system/framework/services.jar"
+
+        cd "$APKTOOL_DIR/system/framework/framework.jar"
+        PATCH="$SRC_DIR/unica/patches/product_feature/mdnie/framework.jar/0001-Disable-HW-mDNIe.patch"
+        OUT="$(patch -p1 -s -t -N --dry-run < "$PATCH")" \
+            || echo "$OUT" | grep -q "Skipping patch" || false
+        patch -p1 -s -t -N --no-backup-if-mismatch < "$PATCH" &> /dev/null || true
+        cd - &> /dev/null
+
+        cd "$APKTOOL_DIR/system/framework/services.jar"
+        PATCH="$SRC_DIR/unica/patches/product_feature/mdnie/services.jar/0001-Disable-HW-mDNIe.patch"
+        OUT="$(patch -p1 -s -t -N --dry-run < "$PATCH")" \
+            || echo "$OUT" | grep -q "Skipping patch" || false
+        patch -p1 -s -t -N --no-backup-if-mismatch < "$PATCH" &> /dev/null || true
+        cd - &> /dev/null
+    fi
+else
+    if $TARGET_HAS_HW_MDNIE; then
+        # TODO: add HW mDNIe support
+        true
     fi
 fi
