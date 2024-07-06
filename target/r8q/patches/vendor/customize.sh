84a52dd334abeb25da13ab51034bde562d405876
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-07-06 12:08:45 +0200

    r8q: fix NFC for G7810/G781N
    Change-Id: I4a92f31c51518bfc4197a3424c03afc3b599f41c

--- target/r8q/patches/vendor/customize.sh
+++ target/r8q/patches/vendor/customize.sh
@@ -87,5 +87,13 @@ if ! grep -q "G781B" "$WORK_DIR/vendor/etc/init/init.nfc.samsung.rc"; then
         echo "on property:ro.boot.em.model=SM-G781B"
         echo "    setprop ro.boot.product.hardware.sku \"s3fwrn5\""
         echo "    setprop ro.vendor.nfc.feature.chipname \"SLSI\""
+        echo ""
+        echo "on property:ro.boot.em.model=SM-G7810"
+        echo "    setprop ro.boot.product.hardware.sku \"s3fwrn5\""
+        echo "    setprop ro.vendor.nfc.feature.chipname \"SLSI\""
+        echo ""
+        echo "on property:ro.boot.em.model=SM-G781N"
+        echo "    setprop ro.boot.product.hardware.sku \"s3fwrn5\""
+        echo "    setprop ro.vendor.nfc.feature.chipname \"SLSI\""
     } >> "$WORK_DIR/vendor/etc/init/init.nfc.samsung.rc"
 fi
