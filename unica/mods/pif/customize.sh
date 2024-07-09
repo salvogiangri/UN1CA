6db2a06b719ed49a9731a8d197672f2c9feba0bf
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-07-09 18:41:52 +0200

    unica: patches: pif: remove additional warranty bit props
    Change-Id: I2e0c1e37f39473bb1cdbcd4eb7d6cc9c6a1a8097

--- unica/mods/pif/customize.sh
+++ unica/mods/pif/customize.sh
@@ -26,8 +26,10 @@ CODENAME="$(GET_PROP "ro.product.system.name" "$WORK_DIR/system/system/build.pro
 {
     echo -e "\n"
     echo    "on property:service.bootanim.exit=1"
+    echo    "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.verifiedbootstate green"
     echo    "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.warranty_bit 0"
-    echo -n "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.verifiedbootstate green"
+    echo    "    exec u:r:init:s0 root root -- /system/bin/rezetprop -d ro.boot.wb.hs"
+    echo -n "    exec u:r:init:s0 root root -- /system/bin/rezetprop -d ro.boot.wb.snapQB"
     if [[ "$(GET_PROP "ro.product.first_api_level" "$WORK_DIR/vendor/build.prop")" -ge "33" ]]; then
         echo -e -n "\n    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.product.first_api_level 32"
     fi
