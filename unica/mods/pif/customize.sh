9a2a415d8705f6570d3a579976b495cc250e93d2
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-07-09 03:20:33 +0200

    unica: patches: pif: no-root rezetprop
    Change-Id: Id31c574e09fbfeef3f600e8df4146d0575a50831

--- unica/mods/pif/customize.sh
+++ unica/mods/pif/customize.sh
@@ -13,7 +13,7 @@ GET_PROP()
 
 if ! grep -q "system/bin/rezetprop" "$WORK_DIR/configs/file_context-system"; then
     {
-        echo "/system/bin/rezetprop u:object_r:system_file:s0"
+        echo "/system/bin/rezetprop u:object_r:init_exec:s0"
     } >> "$WORK_DIR/configs/file_context-system"
 fi
 if ! grep -q "system/bin/rezetprop" "$WORK_DIR/configs/fs_config-system"; then
@@ -25,9 +25,12 @@ fi
 CODENAME="$(GET_PROP "ro.product.system.name" "$WORK_DIR/system/system/build.prop")"
 echo -e "\n" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
 echo    "on property:service.bootanim.exit=1" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
-echo    "    exec u:r:magisk:s0 root root -- /system/bin/rezetprop -n ro.boot.verifiedbootstate green" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
-echo -n "    exec u:r:su:s0 root root -- /system/bin/rezetprop -n ro.boot.verifiedbootstate green" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
+echo -n "    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.boot.verifiedbootstate green" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
 if [[ "$(GET_PROP "ro.product.first_api_level" "$WORK_DIR/vendor/build.prop")" -ge "33" ]]; then
-    echo -e "\n    exec u:r:magisk:s0 root root -- /system/bin/rezetprop -n ro.product.first_api_level 32" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
-    echo -n "    exec u:r:su:s0 root root -- /system/bin/rezetprop -n ro.product.first_api_level 32" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
+    echo -e -n "\n    exec u:r:init:s0 root root -- /system/bin/rezetprop -n ro.product.first_api_level 32" >> "$WORK_DIR/system/system/etc/init/ssu_$CODENAME.rc"
 fi
+
+LINES="$(sed -n "/^(allow init init_exec\b/=" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil")"
+for l in $LINES; do
+    sed -i "${l} s/)))/ execute_no_trans)))/" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
+done
