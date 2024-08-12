74667b85e26c2702eada26922a9c36b0de34848c
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-08-12 16:20:34 +0200

    scripts/build_dependencies.sh: build android-tools with bundled fmt library
    Follow up of the following commit: https://github.com/nmeum/android-tools/commit/337806901ac6269ef4250016c6b12add302b534f
    
    Change-Id: Ia2b8ef09dc1b65681722537b8d97a6f5dc3aca64

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -43,7 +43,7 @@ BUILD_ANDROID_TOOLS()
 
     cd "$SRC_DIR/external/android-tools"
     mkdir -p "build" && cd "build"
-    cmake ..
+    cmake -DANDROID_TOOLS_USE_BUNDLED_FMT=ON ..
     make -j"$JOBS" --quiet
     find "vendor" -maxdepth 1 -type f -exec test -x {} \; -exec cp --preserve=all {} "$TOOLS_DIR" \;
     cd ..
