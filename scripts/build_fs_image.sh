3dbfc76cfe1f6f2ece1e84830c633c7552dd4ec4
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-08-16 23:25:29 +0200

    scripts/build_fs_image.sh: complete overhaul
    - Move to AOSP way of calculating image size
    - Temporarily address f2fs issues (https://github.com/nmeum/android-tools/issues/158)
    
    Change-Id: I085d57a1e1948dc86f59138c1982b11a54bfefc1

--- scripts/build_fs_image.sh
+++ scripts/build_fs_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2023 BlackMesa123
+# Copyright (C) 2024 BlackMesa123
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,35 +18,80 @@
 
 set -eu
 
+# [
+ROUND_UP_TO_4K()
+{
+    local ROUNDED
+    ROUNDED="$(echo "$1 + 4095" | bc -l)"
+    ROUNDED="$(echo "scale=0; $ROUNDED - ($ROUNDED % 4096)" | bc -l)"
+    echo "$ROUNDED"
+}
+
+BUILD_EXT4_IMAGE()
+{
+    local IMG_SIZE="$1"
+    local INODES="$2"
+    local SPARSE="$3"
+
+    if ! grep -q "lost\\\+found" "$FC_FILE"; then
+        if [[ "$PARTITION" == "system" ]]; then
+            echo "/lost\+found u:object_r:rootfs:s0" >> "$FC_FILE"
+        else
+            echo "/$PARTITION/lost\+found $(head -n 1 "$FC_FILE" | cut -d ' ' -f 2)" >> "$FC_FILE"
+        fi
+    fi
+
+    if ! grep -q "lost+found" "$FS_FILE"; then
+        if [[ "$PARTITION" == "system" ]]; then
+            echo "lost+found 0 0 700 capabilities=0x0" >> "$FS_FILE"
+        else
+            echo "$PARTITION/lost+found 0 0 700 capabilities=0x0" >> "$FS_FILE"
+        fi
+    fi
+
+    SPARSE_FLAG=""
+    $SPARSE && SPARSE_FLAG="-s"
+    mkuserimg_mke2fs $SPARSE_FLAG \
+        "$IN_DIR" "$IN_DIR/../$PARTITION.img" ext4 "$MOUNT_POINT" "$IMG_SIZE" \
+        -j 0 -T 1230735600 -C "$FS_FILE" -L "$MOUNT_POINT" \
+        -i "$INODES" -M 0 -I 256 "$FC_FILE"
+    if ! $SPARSE; then
+        e2fsck -f -n "$IN_DIR/../$PARTITION.img"
+    fi
+}
+
+GET_DISK_USAGE()
+{
+    local SIZE
+    SIZE=$(du -b -k -s "$1" | cut -f 1)
+    echo "$SIZE * 1024" | bc -l
+}
+
+GET_INODE_USAGE()
+{
+    local INODES
+    local SPARE_INODES
+
+    INODES=$(find "$1" -print | wc -l)
+    SPARE_INODES=$(echo "scale=0; ($INODES * 6) / 100" | bc -l)
+    [[ "$SPARE_INODES" -lt 12 ]] && SPARE_INODES=12
+
+    echo "$INODES + $SPARE_INODES" | bc -l
+}
+# ]
+
 if [ "$#" != 4 ]; then
     echo "Usage: build_fs_image <fs> <dir> <file_context> <fs_config>"
     exit 1
 fi
-
-[[ $1 == *"sparse" ]] && SPARSE=true || SPARSE=false
-EXT4=false
-F2FS=false
-EROFS=false
-case "$1" in
-    "ext4"*)
-        EXT4=true
-        ;;
-    "f2fs"*)
-        F2FS=true
-        ;;
-    "erofs"*)
-        EROFS=true
-        ;;
-    *)
-        echo "\"$1\" is not valid fs."
-        echo "Available FS:"
-        echo "ext4(+sparse)"
-        echo "f2fs(+sparse)"
-        echo "erofs(+sparse)"
-        exit 1
-        ;;
-esac
-
+if ! echo "$1" | grep -q -w -e "ext4" -e "f2fs" -e "erofs"; then
+    echo "\"$1\" is not valid fs."
+    echo "Available FS:"
+    echo "ext4(+sparse)"
+    echo "f2fs(+sparse)"
+    echo "erofs(+sparse)"
+    exit 1
+fi
 if [ ! -d "$2" ]; then
     echo "Folder not found: $2"
     exit 1
@@ -60,69 +105,80 @@ if [ ! -f "$4" ]; then
     exit 1
 fi
 
+FS_TYPE="$1"
+IN_DIR="$2"
+FC_FILE="$3"
+FS_FILE="$4"
+
+SPARSE=false
+[[ "$1" == *"+sparse" ]] && SPARSE=true
+
 PARTITION=$(basename "$2")
+MOUNT_POINT="$PARTITION"
+[[ "$PARTITION" == "system" ]] && MOUNT_POINT="/"
 
-echo -e "- Building $PARTITION.img as $1...\n"
+echo -e "- Building $PARTITION.img as $FS_TYPE...\n"
 
-if $EXT4; then
-    [[ $PARTITION == "system" ]] && MOUNT_POINT="/" || MOUNT_POINT="$PARTITION"
+case "$FS_TYPE" in
+    "ext4"*)
+        IMG_SIZE="$(GET_DISK_USAGE "$IN_DIR")"
+        IMG_SIZE=$(echo "scale=0; ($IMG_SIZE * 1.1) / 1" | bc -l) # 10% headroom to avoid failures
+        IMG_SIZE="$(echo "$IMG_SIZE + 16777216" | bc -l)" # temporarely add 16MB of reserved space
+        IMG_SIZE="$(ROUND_UP_TO_4K "$IMG_SIZE")"
+        INODES="$(GET_INODE_USAGE "$IN_DIR")"
 
-    IMG_SIZE=$(du -sb "$2" | cut -f 1)
-    IMG_SIZE=$(echo "$IMG_SIZE * 1.01" | bc -l)
-    IMG_SIZE=${IMG_SIZE%.*}
-    [[ "$IMG_SIZE" -lt 2097152 ]] && IMG_SIZE=2097152
+        BUILD_EXT4_IMAGE "$IMG_SIZE" "$INODES" "false"
 
-    INODES=$(find "$2" -print | wc -l)
-    SPARE_INODES=$(echo "$INODES * 6 / 100" | bc -l)
-    SPARE_INODES=${SPARE_INODES%.*}
-    [[ "$SPARE_INODES" -lt 12 ]] && SPARE_INODES=12
+        IMG_INFO="$(tune2fs -l "$IN_DIR/../$PARTITION.img")"
 
-    if ! grep -q "lost\\\+found" "$3"; then
-        [[ $PARTITION == "system" ]] && echo "/lost\+found u:object_r:rootfs:s0" >> "$3"
-        [[ $PARTITION == "odm" ]] && echo "/odm/lost\+found u:object_r:vendor_file:s0" >> "$3"
-        [[ $PARTITION == "product" ]] && echo "/product/lost\+found u:object_r:system_file:s0" >> "$3"
-        [[ $PARTITION == "vendor" ]] && echo "/vendor/lost\+found u:object_r:vendor_file:s0" >> "$3"
-    fi
+        # Reduce img size to minimum (add .3% margin)
+        FREE_SIZE="$(echo "$IMG_INFO" | grep "Free blocks" | tr -s ' ' | cut -f 3 -d ' ')"
+        FREE_SIZE="$(echo "$FREE_SIZE * 4096" | bc -l)"
+        IMG_SIZE="$(echo "$IMG_SIZE - $FREE_SIZE" | bc -l)"
+        IMG_SIZE="$(echo "scale=0; ($IMG_SIZE * 1003) / 1000" | bc -l)"
+        [[ "$IMG_SIZE" -lt 262144 ]] && IMG_SIZE=262144
+        IMG_SIZE="$(ROUND_UP_TO_4K "$IMG_SIZE")"
 
-    if ! grep -q "lost+found" "$4"; then
-        [[ $PARTITION == "system" ]] \
-            && echo "lost+found 0 0 700 capabilities=0x0" >> "$4" || echo "$PARTITION/lost+found 0 0 700 capabilities=0x0" >> "$4"
-    fi
+        # Reduce inodes to minimum (add .2% margin or 1 inode, whichever is greater)
+        INODES="$(echo "$IMG_INFO" | grep "Inode count" | tr -s ' ' | cut -f 3 -d ' ')"
+        FREE_INODES="$(echo "$IMG_INFO" | grep "Free inodes" | tr -s ' ' | cut -f 3 -d ' ')"
+        INODES="$(echo "$INODES - $FREE_INODES" | bc -l)"
+        SPARE_INODES=$(echo "scale=0; ($INODES * 2) / 100" | bc -l)
+        [[ "$SPARE_INODES" -lt 1 ]] && SPARE_INODES=1
+        INODES="$(echo "$INODES + $SPARE_INODES" | bc -l)"
 
-    SPARSE_FLAG=""
-    $SPARSE && SPARSE_FLAG="-s"
-    { mkuserimg_mke2fs $SPARSE_FLAG -j 0 -T 1230735600 -C "$4" \
-        -L "$MOUNT_POINT" -i "$(echo "$INODES + $SPARE_INODES" | bc -l)" -I 256 \
-        "$2" "$2/../$PARTITION.img" ext4 "$MOUNT_POINT" "$IMG_SIZE" "$3" > /dev/null; } 2>&1
-elif $F2FS; then
-    [[ $PARTITION == "system" ]] && MOUNT_POINT="/" || MOUNT_POINT="$PARTITION"
-    IMG_SIZE=$(du -sb "$2" | cut -f 1)
-    IMG_SIZE=$(echo "$IMG_SIZE * 1.11" | bc -l)
-    IMG_SIZE=${IMG_SIZE%.*}
-    [[ "$IMG_SIZE" -lt 56623104 ]] && IMG_SIZE=56623104
+        BUILD_EXT4_IMAGE "$IMG_SIZE" "$INODES" "$SPARSE"
+        ;;
+    "f2fs"*)
+        IMG_SIZE="$(GET_DISK_USAGE "$IN_DIR")"
+        IMG_SIZE=$(echo "scale=0; ($IMG_SIZE * 1.1) / 1" | bc -l) # 10% headroom to avoid failures
+        IMG_SIZE="$(echo "$IMG_SIZE + 16777216" | bc -l)" # temporarely add 16MB of reserved space
+        IMG_SIZE="$(ROUND_UP_TO_4K "$IMG_SIZE")"
+        [[ "$IMG_SIZE" -lt 18882560 ]] && IMG_SIZE=18882560
 
-    if [[ "$PARTITION" != "system" ]]; then
-        sed -i "s/^\/$PARTITION /\/$PARTITION\/$PARTITION /g" "$3"
-    fi
+        if [[ "$PARTITION" != "system" ]]; then
+            sed -i "s/^\/$PARTITION /\/$PARTITION\/$PARTITION /g" "$FC_FILE"
+        fi
 
-    SPARSE_FLAG=""
-    $SPARSE && SPARSE_FLAG="-S"
-    { mkf2fsuserimg "$2/../$PARTITION.img" "$IMG_SIZE" \
-        $SPARSE_FLAG -C "$4" -f "$2" \
-        -s "$3" -t "$MOUNT_POINT" -T 1640995200 \
-        -L "$MOUNT_POINT" --prjquota --compression --readonly > /dev/null; } 2>&1
-elif $EROFS; then
-    [[ $PARTITION == "system" ]] && MOUNT_POINT="/" || MOUNT_POINT="/$PARTITION"
-
-    mkfs.erofs -zlz4hc,9 -b 4096 -T 1640995200 \
-        --mount-point="$MOUNT_POINT" --file-contexts="$3" --fs-config="$4" \
-        "$2/../$PARTITION.img.raw" "$2"
-    if $SPARSE; then
-        img2simg "$2/../$PARTITION.img.raw" "$2/../$PARTITION.img" && rm "$2/../$PARTITION.img.raw"
-    else
-        mv "$2/../$PARTITION.img.raw" "$2/../$PARTITION.img"
-    fi
-fi
+        SPARSE_FLAG=""
+        $SPARSE && SPARSE_FLAG="-S"
+        mkf2fsuserimg "$IN_DIR/../$PARTITION.img" "$IMG_SIZE" \
+            $SPARSE_FLAG -C "$FS_FILE" -f "$IN_DIR" \
+            -s "$FC_FILE" -t "$MOUNT_POINT" -T 1640995200 \
+            -L "$MOUNT_POINT" --prjquota --compression \
+            --readonly --sldc 0 -b 4096
+        ;;
+    "erofs"*)
+        mkfs.erofs -zlz4hc,9 -b 4096 --mount-point="$MOUNT_POINT" \
+            --fs-config-file="$FS_FILE" --file-contexts="$FC_FILE" -T 1640995200 \
+            "$IN_DIR/../$PARTITION.img" "$IN_DIR"
+        fsck.erofs --extract "$IN_DIR/../$PARTITION.img"
+        if $SPARSE; then
+            img2simg "$IN_DIR/../$PARTITION.img" "$IN_DIR/../$PARTITION.img.sparse"
+            mv "$IN_DIR/../$PARTITION.img.sparse" "$IN_DIR/../$PARTITION.img"
+        fi
+        ;;
+esac
 
 echo ""
 exit 0
