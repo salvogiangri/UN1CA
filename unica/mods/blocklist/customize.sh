bac33fc5ab04d39afa0c059740bad7ad06a7f541
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-08-19 01:28:17 +0200

    unica: mods: blocklist: init
    Change-Id: I7c2a0dc724870082236bf00eac175d786d5f1383

---
+++ unica/mods/blocklist/customize.sh
@@ -0,0 +1,25 @@
+REMOVE_FROM_WORK_DIR()
+{
+    local FILE_PATH="$1"
+
+    if [ -e "$FILE_PATH" ]; then
+        local FILE
+        local PARTITION
+        FILE="$(echo -n "$FILE_PATH" | sed "s.$WORK_DIR/..")"
+        PARTITION="$(echo -n "$FILE" | cut -d "/" -f 1)"
+
+        echo "Debloating /$FILE"
+        rm -rf "$FILE_PATH"
+
+        [[ "$PARTITION" == "system" ]] && FILE="$(echo "$FILE" | sed 's.^system/system/.system/.')"
+        FILE="$(echo -n "$FILE" | sed 's/\//\\\//g')"
+        sed -i "/$FILE/d" "$WORK_DIR/configs/fs_config-$PARTITION"
+
+        FILE="$(echo -n "$FILE" | sed 's/\./\\\\\./g')"
+        sed -i "/$FILE/d" "$WORK_DIR/configs/file_context-$PARTITION"
+    fi
+}
+
+sed -i "s/ldu_blocklist/unica_blocklist/g" "$WORK_DIR/configs/file_context-system"
+sed -i "s/ldu_blocklist/unica_blocklist/g" "$WORK_DIR/configs/fs_config-system"
+REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/ldu_blocklist.xml"
