90a1697ebc624619d90ac59ee8f7de91bdba039d
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-09-24 00:57:36 +0200

    target: patches: miscs: minor adjustments

--- target/r0q/patches/miscs/customize.sh
+++ target/r0q/patches/miscs/customize.sh
@@ -42,5 +42,4 @@ echo "Fix up /product/etc/build.prop"
 sed -i "/# Removed by /d" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
-    && sed -i "/# Value overridden /d" "$WORK_DIR/product/etc/build.prop" \
     && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/product/etc/build.prop"
