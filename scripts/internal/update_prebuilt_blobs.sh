750d8e22d255a0b402c2a75b5df07c153310a2c2
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-09-25 11:59:33 +0200

    b0q: init

--- scripts/internal/update_prebuilt_blobs.sh
+++ scripts/internal/update_prebuilt_blobs.sh
@@ -90,6 +90,11 @@ case "$1" in
         BLOBS+="$(find "$SRC_DIR/target/a71/patches/stock_blobs/system_ext" -type f -printf "\n%p" \
             | sed "s.$SRC_DIR/target/a71/patches/stock_blobs.system/system.")"
         ;;
+    "target/b0q/patches/ultra")
+        MODULE="$1"
+        FW="SM-S918B/EUX/350196551234562"
+        BLOBS="$(find "$SRC_DIR/target/b0q/patches/ultra/system" -type f | sed "s.$SRC_DIR/target/b0q/patches/ultra.system.")"
+        ;;
     "target/m52xq/patches/stock_blobs")
         MODULE="$1"
         FW="SM-A528B/BTU/352599501234566"
--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -1,19 +1,24 @@
 SKIPUNZIP=1
 
+WEBP_RES="2400"
+MP4_RES="1080:-1"
+[[ "$TARGET_CODENAME" == "b0q" ]] && WEBP_RES="3088"
+[[ "$TARGET_CODENAME" == "b0q" ]] && MP4_RES="1440:-1"
+
 echo "Resize wallpaper-res.apk"
 if [ ! -d "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk" ]; then
     bash "$SRC_DIR/scripts/apktool.sh" d "system/priv-app/wallpaper-res/wallpaper-res.apk"
 fi
 for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/drawable-nodpi/Wallpaper_"*.webp
 do
-    cwebp -q 100 -resize 2400 2400 "$f" -o "$(dirname "$f")/temp.webp" &> /dev/null
+    cwebp -q 100 -resize $WEBP_RES $WEBP_RES "$f" -o "$(dirname "$f")/temp.webp" &> /dev/null
     mv -f "$(dirname "$f")/temp.webp" "$f"
 done
 for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/raw/"*.mp4
 do
     ffmpeg -i "$f" -c:v libx265 -tag:v hvc1 -c:a copy \
         -crf 18 -movflags use_metadata_tags -map_metadata 0 \
-        -vf "scale=-1:2400,setsar=1:1" -video_track_timescale 60000 \
+        -vf "scale=$MP4_RES,setsar=1:1" -video_track_timescale 60000 \
         "$(dirname "$f")/temp.mp4" &> /dev/null
     mv -f "$(dirname "$f")/temp.mp4" "$f"
 done
