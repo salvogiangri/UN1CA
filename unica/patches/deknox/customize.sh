a5f01657285804709f8ecd0e7aaa7ea245455831
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-10-08 09:44:35 +0200

    unica: patches: deknox: nuke Knox ZT, minor adjustments
    Change-Id: I8e3e618e1212654d1c82e3238a3e14055fd3c35c

--- unica/patches/deknox/customize.sh
+++ unica/patches/deknox/customize.sh
@@ -1,4 +1,53 @@
 # [
+GET_PROP()
+{
+    local PROP="$1"
+    local FILE="$2"
+
+    if [ ! -f "$FILE" ]; then
+        echo "File not found: $FILE"
+        exit 1
+    fi
+
+    grep "^$PROP=" "$FILE" | cut -d "=" -f2-
+}
+
+SET_PROP()
+{
+    local PROP="$1"
+    local VALUE="$2"
+    local FILE="$3"
+
+    if [ ! -f "$FILE" ]; then
+        echo "File not found: $FILE"
+        return 1
+    fi
+
+    if [[ "$2" == "-d" ]] || [[ "$2" == "--delete" ]]; then
+        PROP="$(echo -n "$PROP" | sed 's/=//g')"
+        if grep -Fq "$PROP" "$FILE"; then
+            echo "Deleting \"$PROP\" prop in $FILE" | sed "s.$WORK_DIR..g"
+            sed -i "/^$PROP/d" "$FILE"
+        fi
+    else
+        if grep -Fq "$PROP" "$FILE"; then
+            local LINES
+
+            echo "Replacing \"$PROP\" prop with \"$VALUE\" in $FILE" | sed "s.$WORK_DIR..g"
+            LINES="$(sed -n "/^${PROP}\b/=" "$FILE")"
+            for l in $LINES; do
+                sed -i "$l c${PROP}=${VALUE}" "$FILE"
+            done
+        else
+            echo "Adding \"$PROP\" prop with \"$VALUE\" in $FILE" | sed "s.$WORK_DIR..g"
+            if ! grep -q "Added by scripts" "$FILE"; then
+                echo "# Added by scripts/internal/apply_modules.sh" >> "$FILE"
+            fi
+            echo "$PROP=$VALUE" >> "$FILE"
+        fi
+    fi
+}
+
 REMOVE_FROM_WORK_DIR()
 {
     local FILE_PATH="$1"
@@ -22,11 +71,16 @@ REMOVE_FROM_WORK_DIR()
 }
 # ]
 
+if [ -n "$(GET_PROP "ro.security.fips.ux" "$WORK_DIR/vendor/build.prop")" ]; then
+    SET_PROP "ro.security.fips.ux" "Disabled" "$WORK_DIR/vendor/build.prop"
+fi
+
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/app/BlockchainBasicKit"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/bin/dualdard"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/init/dualdard.rc"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.android.hdmapp.xml"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.android.kgclient.xml"
+REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.android.knox.zt.framework.xml"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/sysconfig/preinstalled-packages-com.samsung.android.coldwalletservice.xml"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/framework/service-samsung-blockchain.jar"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/android.hardware.weaver@1.0.so"
@@ -50,3 +104,4 @@ REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/vendor.samsung.hardware.tlc.
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/priv-app/HdmApk"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/priv-app/KnoxAIFrameworkApp"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/priv-app/KnoxGuard"
+REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/priv-app/KnoxZtFramework"
