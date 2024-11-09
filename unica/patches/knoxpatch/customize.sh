e1e93da843b5fc6d2e03b915ef8912f3336b4069
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-11-09 13:12:34 +0100

    unica: patches: knoxpatch: SCPM/KmxService patches
    Change-Id: If2372b6691745eb6d141c844a639989b13c1a53c

--- unica/patches/knoxpatch/customize.sh
+++ unica/patches/knoxpatch/customize.sh
@@ -1,5 +1,31 @@
 SKIPUNZIP=1
 
+# [
+DECOMPILE()
+{
+    if [ ! -d "$APKTOOL_DIR/$1" ]; then
+        bash "$SRC_DIR/scripts/apktool.sh" d "$1"
+    fi
+}
+
+APPLY_PATCH()
+{
+    local PATCH
+    local COMMIT_NAME
+    local OUT
+
+    DECOMPILE "$1"
+
+    cd "$APKTOOL_DIR/$1"
+    PATCH="$SRC_DIR/unica/patches/knoxpatch/$2"
+    COMMIT_NAME="$(grep "^Subject:" "$PATCH" | sed 's/.*PATCH] //')"
+    echo "Applying \"$COMMIT_NAME\" to /$1"
+    OUT="$(patch -p1 -s -t -N --dry-run < "$PATCH")" \
+        || echo "$OUT" | grep -q "Skipping patch" || false
+    patch -p1 -s -t -N --no-backup-if-mismatch < "$PATCH" &> /dev/null || true
+    cd - &> /dev/null
+}
+
 REMOVE_FROM_WORK_DIR()
 {
     local FILE_PATH="$1"
@@ -21,9 +47,14 @@ REMOVE_FROM_WORK_DIR()
         sed -i "/$FILE /d" "$WORK_DIR/configs/file_context-$PARTITION"
     fi
 }
+# ]
 
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/etc/public.libraries-wsm.samsung.txt"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/libhal.wsm.samsung.so"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib/vendor.samsung.hardware.security.wsm.service-V1-ndk.so"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/libhal.wsm.samsung.so"
 REMOVE_FROM_WORK_DIR "$WORK_DIR/system/system/lib64/vendor.samsung.hardware.security.wsm.service-V1-ndk.so"
+
+if [ -f "$WORK_DIR/system/system/priv-app/KmxService/KmxService.apk" ]; then
+    APPLY_PATCH "system/priv-app/KmxService/KmxService.apk" "kmx/KmxService.apk/0001-Nuke-ROT-IntegrityStatus-check.patch"
+fi
