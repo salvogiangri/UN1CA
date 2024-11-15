748bd451d9fe46c0dcbed526c061c9012d823e21
ata-kaner <ata.kaner03@gmail.com>
2024-11-15 16:31:16 +0300

    x1q: init

---
+++ target/x1q/config.sh
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S20 5G (Qualcomm) (x1q)
+TARGET_NAME="Galaxy S20 5G (Qualcomm)"
+TARGET_CODENAME="x1q"
+TARGET_FIRMWARE="SM-G981N/KOO/355995110205095"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_API_LEVEL=33
+TARGET_PRODUCT_FIRST_API_LEVEL=29
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="qssi"
+TARGET_OS_FILE_SYSTEM="ext4"
+TARGET_SUPER_PARTITION_SIZE=10292822016
+TARGET_SUPER_GROUP_SIZE=10288627712
+TARGET_HAS_SYSTEM_EXT=false
+# SEC Product Feature
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+TARGET_AUTO_BRIGHTNESS_TYPE="4"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_sdm8250_xx"
+TARGET_ESE_CHIP_VENDOR="NXP"
+TARGET_ESE_COS_NAME="JCOP5.2T"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_ultrasonic"
+TARGET_HAS_HW_MDNIE=true
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,96,120"
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_MDNIE_SUPPORT_HDR_EFFECT=true
+TARGET_MDNIE_SUPPORTED_MODES="65303"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+TARGET_MULTI_MIC_MANAGER_VERSION="08020"
+TARGET_SSRM_CONFIG_NAME="siop_x1q_sm8250"
+TARGET_SUPPORT_CUTOUT_PROTECTION=true
