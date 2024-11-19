c9d7f14b5e39e0998aad0e5be629ca32a7a951c9
ata-kaner <ata.kaner03@gmail.com>
2024-11-20 00:10:52 +0300

    unica: mods: tweaks: Simplify QHD display logic
    Change-Id: I86bfe92859c9cda0d1352dd52b78f7d737ad93da

--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -2,8 +2,17 @@ SKIPUNZIP=1
 
 WEBP_RES="2400"
 MP4_RES="1080:-1"
-[[ "$TARGET_CODENAME" == "b0q" ]] || [[ "$TARGET_CODENAME" == "dm3q" ]] && WEBP_RES="3088"
-[[ "$TARGET_CODENAME" == "b0q" ]] || [[ "$TARGET_CODENAME" == "dm3q" ]] && MP4_RES="1440:-1"
+
+if ! $SOURCE_HAS_QHD_DISPLAY; then
+    if $TARGET_HAS_QHD_DISPLAY; then
+        if [[ "$TARGET_PRODUCT_FIRST_API_LEVEL" -le 30 ]]; then
+            WEBP_RES="3200"
+        else
+            WEBP_RES="3088"
+        fi
+        MP4_RES="1440:-1"
+    fi
+fi
 
 echo "Resize wallpaper-res.apk"
 if [ ! -d "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk" ]; then
