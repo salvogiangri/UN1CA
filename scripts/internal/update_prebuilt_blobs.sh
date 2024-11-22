b97622d186051c62e4cf6dbbceffaf6156ec6ba5
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-11-22 23:30:52 +0100

    unica: patches: ultra: also for ESSI. NEED BLOB FETCH

--- scripts/internal/update_prebuilt_blobs.sh
+++ scripts/internal/update_prebuilt_blobs.sh
@@ -71,10 +71,15 @@ case "$1" in
         BLOBS="$(find "$SRC_DIR/unica/patches/product_feature/fingerprint/side_fp/system" -type f \
             | sed "s.$SRC_DIR/unica/patches/product_feature/fingerprint/side_fp.system.")"
         ;;
-    "unica/patches/ultra")
+    "unica/patches/ultra/qssi")
         MODULE="$1"
         FW="SM-S918B/EUX/350196551234562"
-        BLOBS="$(find "$SRC_DIR/unica/patches/ultra/system" -type f | sed "s.$SRC_DIR/unica/patches/ultra.system.")"
+        BLOBS="$(find "$SRC_DIR/unica/patches/ultra/qssi/system" -type f | sed "s.$SRC_DIR/unica/patches/ultra/qssi.system.")"
+        ;;
+    "unica/patches/ultra/essi")
+        MODULE="$1"
+        FW="SM-S908B/EUX/350048581234569"
+        BLOBS="$(find "$SRC_DIR/unica/patches/ultra/system/essi" -type f | sed "s.$SRC_DIR/unica/patches/ultra/essi.system.")"
         ;;
     "unica/patches/uwb/qssi")
         MODULE="$1"
