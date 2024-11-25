ba0d68749474d69f80d54e4f793c46e5ece3de2f
Salvo Giangreco <giangrecosalvo9@gmail.com>
2024-11-25 14:02:08 +0100

    unica: mods: bootanim: warn when target res is not set
    Change-Id: I4310f03e15409496efdcc6179f21fc678aa7aa33

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -7,6 +7,8 @@ if echo "$TWOTHREE_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2340x1080/"* "$WORK_DIR/system/system/media"
 elif echo "$TWOFOUR_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2400x1080/"* "$WORK_DIR/system/system/media"
+else
+    echo "Unknown boot animation resolution for \"$TARGET_CODENAME\""
 fi
 
 cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/system/"* "$WORK_DIR/system/system"
