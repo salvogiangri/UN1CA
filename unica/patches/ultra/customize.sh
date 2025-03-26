766192d8a3ae3d45ac7cdbc5854f54043d61e825
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-03-26 15:07:48 +0100

    unica: patches: ultra: implement support for essi

--- unica/patches/ultra/customize.sh
+++ unica/patches/ultra/customize.sh
@@ -1,26 +1,32 @@
 TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 
 if [ -d "$TARGET_FIRMWARE_PATH/system/system/media/audio/pensounds" ]; then
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/app/AirGlance/AirGlance.apk" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/app/LiveDrawing/LiveDrawing.apk" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" \
+    if [[ "$TARGET_SINGLE_SYSTEM_IMAGE" == "qssi" ]]; then
+        SOURCE="dm3qxxx"
+    elif [[ "$TARGET_SINGLE_SYSTEM_IMAGE" == "essi" ]]; then
+        SOURCE="b0sxxx"
+    fi
+
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/app/AirGlance/AirGlance.apk" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/app/LiveDrawing/LiveDrawing.apk" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" \
         "system/etc/default-permissions/default-permissions-com.samsung.android.service.aircommand.xml" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" \
+    ADD_TO_WORK_DIR "$SOURCE" "system" \
         "system/etc/permissions/privapp-permissions-com.samsung.android.app.readingglass.xml" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" \
+    ADD_TO_WORK_DIR "$SOURCE" "system" \
         "system/etc/permissions/privapp-permissions-com.samsung.android.service.aircommand.xml" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" \
+    ADD_TO_WORK_DIR "$SOURCE" "system" \
         "system/etc/permissions/privapp-permissions-com.samsung.android.service.airviewdictionary.xml" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/etc/sysconfig/airviewdictionaryservice.xml" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/etc/public.libraries-smps.samsung.txt" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib/libandroid_runtime.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib/libsmpsft.smps.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib64/libandroid_runtime.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/lib64/libsmpsft.smps.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/media/audio/pensounds" 0 0 755 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/AirCommand/AirCommand.apk" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/AirReadingGlass/AirReadingGlass.apk" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/SmartEye/SmartEye.apk" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/etc/sysconfig/airviewdictionaryservice.xml" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/etc/public.libraries-smps.samsung.txt" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/lib/libandroid_runtime.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/lib/libsmpsft.smps.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/lib64/libandroid_runtime.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/lib64/libsmpsft.smps.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/media/audio/pensounds" 0 0 755 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/priv-app/AirCommand/AirCommand.apk" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/priv-app/AirReadingGlass/AirReadingGlass.apk" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$SOURCE" "system" "system/priv-app/SmartEye/SmartEye.apk" 0 0 644 "u:object_r:system_file:s0"
 else
     echo "Target device is not an Ultra variant. Ignoring"
 fi
