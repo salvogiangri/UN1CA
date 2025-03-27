b8e98de38627bc09d6bd25a0140a9f1f70071457
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-03-27 16:19:27 +0100

    unica: mods: eureka: improve and reduce warnings
    ESSI doesn't have the ACH ringtones. Also there is no reason to delete only some
    of the unused ringtones as we are replacing the entire media folder with S24
    one. This allows in 1 line to support both essi and qssi

--- unica/mods/eureka/customize.sh
+++ unica/mods/eureka/customize.sh
@@ -1,12 +1,4 @@
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Calm/ACH_The_Voyage.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Calm/ACH_The_Voyage_acoustic.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Calm/ACH_The_Voyage_epic.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Fun/ACH_The_Voyage_EDM.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Fun/ACH_The_Voyage_RnB.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Fun/ACH_The_Voyage_jazz_piano.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Galaxy/ACH_Over_the_Horizon.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Retro/ACH_The_Voyage_pop_remix.ogg"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones/SoundTheme/Retro/ACH_The_Voyage_smooth_remix.ogg"
+DELETE_FROM_WORK_DIR "system" "system/media"
 
 ADD_TO_WORK_DIR "e1qzcx" "system" \
     "system/etc/permissions/privapp-permissions-com.samsung.android.sead.xml" 0 0 644 "u:object_r:system_file:s0"
