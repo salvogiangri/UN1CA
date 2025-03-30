8a1499dc1631d5fc793f4b4829b3f3ec39f4bdab
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-03-30 13:03:58 +0200

    unica: debloat: dynamically delete dexpreopt files
    Change-Id: I13ac6f9ce111e03315f6e08fbd1efd043614d155

--- unica/patches/debloat/customize.sh
+++ unica/patches/debloat/customize.sh
@@ -1,3 +1,24 @@
+# Dexpreopt
+for f in $(find "$WORK_DIR/product" -type d -name "oat"); do
+    DELETE_FROM_WORK_DIR "product" "${f//$WORK_DIR\/product\//}"
+done
+for f in $(find "$WORK_DIR/system" -type d -name "oat"); do
+    DELETE_FROM_WORK_DIR "system" "${f//$WORK_DIR\/system\//}"
+done
+DELETE_FROM_WORK_DIR "system" "system/etc/boot-image.bprof"
+DELETE_FROM_WORK_DIR "system" "system/etc/boot-image.prof"
+DELETE_FROM_WORK_DIR "system" "system/framework/arm"
+DELETE_FROM_WORK_DIR "system" "system/framework/arm64"
+for f in $(find "$WORK_DIR/system/system/framework" -type f -name "*.vdex"); do
+    DELETE_FROM_WORK_DIR "system" "${f//$WORK_DIR\/system\//}"
+done
+if $TARGET_HAS_SYSTEM_EXT; then
+    for f in $(find "$WORK_DIR/system_ext" -type d -name "oat"); do
+        DELETE_FROM_WORK_DIR "system_ext" "${f//$WORK_DIR\/system_ext\//}"
+    done
+fi
+
+# ROM & device-specific debloat list
 [ -f "$SRC_DIR/unica/debloat.sh" ] && source "$SRC_DIR/unica/debloat.sh"
 [ -f "$SRC_DIR/target/$TARGET_CODENAME/debloat.sh" ] && source "$SRC_DIR/target/$TARGET_CODENAME/debloat.sh"
 
