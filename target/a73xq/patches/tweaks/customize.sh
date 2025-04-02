37e6619102c4204244398eb6c0940d7d0d9fd888
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-02 14:00:30 +0200

    target: Add common target support
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- target/a73xq/patches/tweaks/customize.sh
+++
@@ -1,6 +0,0 @@
-SKIPUNZIP=1
-
-LINE="$(sed -n '/\/dev\/cpuset\/background\/cpus/=' "$WORK_DIR/vendor/bin/init.kernel.post_boot-yupik.sh")"
-sed -i \
-    "$LINE cecho 0-1 > /dev/cpuset/background/cpus\necho 0-3 > /dev/cpuset/restricted/cpus" \
-    "$WORK_DIR/vendor/bin/init.kernel.post_boot-yupik.sh"
