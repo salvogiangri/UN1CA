8f8d87a8877eb12f37c98389766e1ed479521f6e
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-04-14 11:25:02 +0200

    scripts: build_utils: build dependencies if required
    Change-Id: I1791ad6d3ea0b45ecb17ce49214c13125ccba43c

--- external/make.sh
+++ external/make.sh
@@ -116,12 +116,6 @@ IS_WSL()
 }
 # ]
 
-if [ "$#" -gt 0 ]; then
-    echo "Usage: $(basename "$0" | sed 's/build_dependencies.sh/build_dependencies/')" >&2
-    echo "This cmd does not accepts any arguments." >&2
-    exit 1
-fi
-
 SRC_DIR="$(GET_SRC_DIR)"
 if [ ! "$SRC_DIR" ]; then
     echo "Couldn't locate the top of the tree. Try setting SRC_DIR." >&2
@@ -173,6 +167,24 @@ SMALI_EXEC=(
     "android-smali.jar" "baksmali" "smali" "smali-baksmali.jar"
 )
 CHECK_TOOLS "${SMALI_EXEC[@]}" && SMALI=false
+
+if [[ "$1" == "--check-tools" ]]; then
+    if ! $ANDROID_TOOLS && \
+            ! $APKTOOL && \
+            ! $EROFS_UTILS && \
+            ! $IMG2SDAT && \
+            ! $SAMFIRM && \
+            ! $SIGNAPK && \
+            ! $SMALI; then
+        exit 0
+    else
+        exit 1
+    fi
+elif [ "$1" ]; then
+    echo "Usage: $(basename "$0" | sed 's/build_dependencies.sh/build_dependencies/')" >&2
+    echo "This script does not accept any arguments." >&2
+    exit 1
+fi
 
 if $ANDROID_TOOLS; then
     ANDROID_TOOLS_CMDS=(
--- scripts/utils/build_utils.sh
+++ scripts/utils/build_utils.sh
@@ -14,3 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
+if ! "$SRC_DIR/external/make.sh" --check-tools; then
+    echo -e '\033[1;37m'"Building required tools..."'\033[0m'
+    "$SRC_DIR/external/make.sh"
+fi
