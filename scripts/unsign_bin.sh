7fce90a209890999901d3111564fd072314c034a
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-04-18 11:12:05 +0200

    scripts/unsign_bin.sh: minor revampment
    Change-Id: I02777900baec27366a8e52306c2df6a8a77c000a

--- scripts/unsign_bin.sh
+++ scripts/unsign_bin.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Copyright (C) 2023 Salvo Giangreco
 #
@@ -16,33 +16,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-set -eu
+# [
+source "$SRC_DIR/scripts/utils/log_utils.sh"
+# ]
 
 if [ "$#" == 0 ]; then
-    echo "Usage: unsign_bin <image> (<image>...)"
+    echo "Usage: unsign_bin <image> (<image>...)" >&2
     exit 1
 fi
 
 while [ "$#" != 0 ]; do
     if [ ! -f "$1" ]; then
-        echo "File not found: $1"
+        LOGE "File not found: $1"
         exit 1
     else
-        if avbtool info_image --image "$1" &>/dev/null; then
-            echo "Removing AVB footer signature"
+        if avbtool info_image --image "$1" &> /dev/null; then
+            LOG "- Removing AVB footer signature from $(basename "$1")"
             avbtool erase_footer --image "$1"
         fi
         if head "$1" | grep -q "SignerVer"; then
-            echo "Removing Samsung header signature"
-            dd if="/dev/zero" of="$1" bs=1 seek=0 count=256 conv=notrunc &>/dev/null
-            dd if="/dev/zero" of="$1" bs=1 seek=768 count=256 conv=notrunc &>/dev/null
+            LOG "- Removing Samsung header signature from $(basename "$1")"
+            dd if="/dev/zero" of="$1" bs=256 seek=0 count=1 conv=notrunc &> /dev/null
+            dd if="/dev/zero" of="$1" bs=256 seek=3 count=1 conv=notrunc &> /dev/null
         fi
         if tail "$1" | grep -q "SignerVer02"; then
-            echo "Removing Samsung footer signature"
+            LOG "- Removing Samsung footer signature from $(basename "$1")"
             truncate -s -512 "$1"
         fi
         if tail "$1" | grep -q "SignerVer03"; then
-            echo "Removing Samsung footer signature"
+            LOG "- Removing Samsung footer signature from $(basename "$1")"
             truncate -s -784 "$1"
         fi
     fi
