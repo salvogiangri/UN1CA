3d6d121b6632f43852901bc59be539c1051f1a9d
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2025-05-11 10:46:33 +0200

    unica: patches: vndk: support for devices without vndk (#439)
    * unica: patches: vndk: support for devices without vndk
    
    Co-authored-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
    
    * unica: patches: vndk: rename from VNDK_VERSION to VENDOR_API_LEVEL
    
    Android 15 deprecates VNDK. This also makes more clear and allows the usage
    of one variable for multiple future usages.
    Vendor API >= 35 means not VNDK.
    Vendor API <= 34 means VNDK.
    
    Change-Id: Ie8d9bd4e07927738a21a78cb732f011d67707ac7
    
    * unica: patches: vndk: require VENDOR_API_LEVEL to be set
    
    Change-Id: I76292096480d595e40287f7a060586b94381151b
    
    * unica: patches: vndk: minor adjustments
    
    Change-Id: I8aab8c8ce5109867c9593cddf74630cc19bf152c
    
    ---------
    
    Co-authored-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -1,27 +1,45 @@
-if [[ "$SOURCE_VNDK_VERSION" != "$TARGET_VNDK_VERSION" ]]; then
+# [
+ADD_TARGET_VNDK_APEX() {
+    case "$TARGET_VENDOR_API_LEVEL" in
+        "30")
+            ADD_TO_WORK_DIR "a73xqxx" "system_ext" "apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
+            ;;
+        "31")
+            ADD_TO_WORK_DIR "r0qxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
+            ;;
+        "33")
+            ADD_TO_WORK_DIR "dm3qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
+            ;;
+        *)
+            LOGE "No APEX blob available for VNDK $TARGET_VENDOR_API_LEVEL"
+            return 1
+            ;;
+    esac
+}
+# ]
+
+if [[ "$SOURCE_VENDOR_API_LEVEL" != "$TARGET_VENDOR_API_LEVEL" ]]; then
     if $TARGET_HAS_SYSTEM_EXT; then
         SYS_EXT_DIR="$WORK_DIR/system_ext"
     else
         SYS_EXT_DIR="$WORK_DIR/system/system/system_ext"
     fi
 
-    if [ ! -f "$SYS_EXT_DIR/apex/com.android.vndk.v$TARGET_VNDK_VERSION.apex" ]; then
-        DELETE_FROM_WORK_DIR "system_ext" "apex/com.android.vndk.v$SOURCE_VNDK_VERSION.apex"
-        case "$TARGET_VNDK_VERSION" in
-            "30")
-                ADD_TO_WORK_DIR "a73xqxx" "system_ext" "apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
-                ;;
-            "31")
-                ADD_TO_WORK_DIR "r0qxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
-                ;;
-            "33")
-                ADD_TO_WORK_DIR "dm3qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
-                ;;
-        esac
-        sed -i "s/version>$SOURCE_VNDK_VERSION/version>$TARGET_VNDK_VERSION/g" "$SYS_EXT_DIR/etc/vintf/manifest.xml"
-    else
-        echo "VNDK v$TARGET_VNDK_VERSION apex is already in place. Ignoring"
+    if [ "$SOURCE_VENDOR_API_LEVEL" -gt "34" ] && [ "$TARGET_VENDOR_API_LEVEL" -gt "34" ]; then
+        :
+    elif [ "$SOURCE_VENDOR_API_LEVEL" -gt "34" ] && [ "$TARGET_VENDOR_API_LEVEL" -le "34" ]; then
+        ADD_TARGET_VNDK_APEX
+        sed -i '$d' "$SYS_EXT_DIR/etc/vintf/manifest.xml"
+        echo "    <vendor-ndk>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
+        echo "        <version>$TARGET_VENDOR_API_LEVEL</version>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
+        echo "    </vendor-ndk>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
+        echo "</manifest>" >> "$SYS_EXT_DIR/etc/vintf/manifest.xml"
+    elif [ "$SOURCE_VENDOR_API_LEVEL" -le "34" ] && [ "$TARGET_VENDOR_API_LEVEL" -gt "34" ]; then
+        DELETE_FROM_WORK_DIR "system_ext" "apex/com.android.vndk.v$SOURCE_VENDOR_API_LEVEL.apex"
+        sed -i -e "/vendor-ndk/d" -e "/version>/d" "$SYS_EXT_DIR/etc/vintf/manifest.xml"
+    elif [ ! -f "$SYS_EXT_DIR/apex/com.android.vndk.v$TARGET_VENDOR_API_LEVEL.apex" ]; then
+        DELETE_FROM_WORK_DIR "system_ext" "apex/com.android.vndk.v$SOURCE_VENDOR_API_LEVEL.apex"
+        ADD_TARGET_VNDK_APEX
+        sed -i "s/version>$SOURCE_VENDOR_API_LEVEL/version>$TARGET_VENDOR_API_LEVEL/g" "$SYS_EXT_DIR/etc/vintf/manifest.xml"
     fi
-else
-    echo "SOURCE_VNDK_VERSION and TARGET_VNDK_VERSION are the same. Ignoring"
 fi
