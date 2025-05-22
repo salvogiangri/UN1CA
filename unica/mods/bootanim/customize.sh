bec50e08a952cde1ddba5550671d7f33b2ff7379
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-05-23 00:11:22 +0600

    Update customize.sh

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,5 +1,5 @@
 TWOTHREE_TARGETS="dm1q dm2q g0q r0q"
-TWOFOUR_TARGETS="a52q a52xq a52sxq a71 a72q a73xq m52xq r8q r9q r9q2"
+TWOFOUR_TARGETS="a52q a52xq a52sxq a71 m51 a72q a73xq m52xq r8q r9q r9q2"
 
 if echo "$TWOTHREE_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2340x1080/"* "$WORK_DIR/system/system/media"
