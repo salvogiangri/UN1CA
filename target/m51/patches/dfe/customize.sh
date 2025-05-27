3ac05c7b9aceb89f488e7968efab81b1a98a4e73
Mehedi H Joy <78579303+mehedihjoy0@users.noreply.github.com>
2025-05-27 16:02:29 +0600

    Update customize.sh

--- target/m51/patches/dfe/customize.sh
+++ target/m51/patches/dfe/customize.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+# UN1CA-M51 Encryption Disabler
 
+VENDOR_ETC="${WORK_DIR}/vendor/etc"
+
+# Atomic fstab patching with all encryption types
+patch_fstab() {
+    local file="$1"
+    [ -f "${file}" ] || return 1
+
+    # Comprehensive encryption removal
+    sed -i -E '
+        s/(,|^)(forceencrypt|fileencryption|fbe|fde|inlinecrypt)=/encryptable=/g;
+        s/,((file|metadata)_encryption|keydirectory|fsverity|forcefdeorfbe|avb|verify|support_scfs|quota)=[^[:space:]]+//g;
+        s/,,+/,/g; s/,[[:space:]]*$//;
+    ' "${file}" && chmod 644 "${file}"
+}
+
+# Execute patches
+echo "Patching encryption configurations..."
+patch_fstab "${VENDOR_ETC}/fstab.default"
+patch_fstab "${VENDOR_ETC}/fstab.emmc"
+
+echo "Encryption disabled in fstab files"
+exit 0
