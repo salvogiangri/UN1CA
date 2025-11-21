bd71436dea83710cdee8553ee8af66f5830b0193
Ferdinand Saurenbach <ferdinand@fsrb.de>
2025-11-21 17:30:48 +0100

    external: Use bundled fmtlib for non action builds too
    Signed-off-by: Ferdinand Saurenbach <ferdinand@fsrb.de>

--- external/make.sh
+++ external/make.sh
@@ -185,7 +185,7 @@ fi
 if $ANDROID_TOOLS; then
     ANDROID_TOOLS_CMDS=(
         "git submodule foreach --recursive \"git am --abort || true\""
-        "cmake -B \"build\" $(GET_CMAKE_FLAGS) -DANDROID_TOOLS_USE_BUNDLED_FMT=$([ "$GITHUB_ACTIONS" ] && echo "ON" || echo "OFF") -DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON"
+        "cmake -B \"build\" $(GET_CMAKE_FLAGS) -DANDROID_TOOLS_USE_BUNDLED_FMT=$([ "$GITHUB_ACTIONS" ] && echo "ON" || echo "ON") -DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON"
         "make -C \"build\" -j\"$(nproc)\""
         "find \"build/vendor\" -maxdepth 1 -type f -exec test -x {} \; -exec cp -a {} \"$TOOLS_DIR/bin\" \;"
         "cp -a \"vendor/avb/avbtool.py\" \"$TOOLS_DIR/bin/avbtool\""
