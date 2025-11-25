3768c0ff9da6febb50f57f41ef4a6e2d73470792
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-11-25 13:02:12 +0000

    a72q: miscs: Disable several GMS component
    Co-authored-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- target/a72q/patches/miscs/customize.sh
+++ target/a72q/patches/miscs/customize.sh
@@ -1,3 +1,10 @@
 LOG_STEP_IN "- Uprev radio HAL to 1.5"
 sed -i "s/1.4::IRadio/1.5::IRadio/g" "$WORK_DIR/vendor/etc/vintf/manifest.xml"
 LOG_STEP_OUT
+
+LOG_STEP_IN "- Disable several GMS components"
+echo "# Disable GMS components" >> "$WORK_DIR/vendor/bin/init.qcom.early_boot.sh"
+echo "pm disable com.google.android.gms/com.google.android.gms.auth.managed.admin.DeviceAdminReceiver" >> "$WORK_DIR/vendor/bin/init.qcom.early_boot.sh"
+echo "pm disable com.google.android.gms/com.google.android.gms.chimera.GmsIntentOperationService" >> "$WORK_DIR/vendor/bin/init.qcom.early_boot.sh"
+echo "pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver" >> "$WORK_DIR/vendor/bin/init.qcom.early_boot.sh"
+LOG_STEP_OUT
