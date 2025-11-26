56b675d72af166a271360fdc43fb879c35f2faa4
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-11-26 17:22:04 +0000

    a72q: camera: Fix MIDAS model detection
    - Fix these fatal issue.
    F DEBUG   : Cmdline: /vendor/bin/hw/vendor.samsung.hardware.snap-service
    F DEBUG   : pid: 1068, tid: 1068, name: binder:1068_2  >>> /vendor/bin/hw/vendor.samsung.hardware.snap-service <<<
    F DEBUG   : uid: 1000
    F DEBUG   : signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x0000000000000000
    F DEBUG   : Cause: null pointer dereference
    F DEBUG   : Abort message: 'Failed to map model.'
    
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- target/a72q/patches/camera/customize.sh
+++ target/a72q/patches/camera/customize.sh
@@ -13,6 +13,9 @@ ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:ob
 DELETE_FROM_WORK_DIR "vendor" "etc/midas"
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/midas" 0 2000 755 "u:object_r:vendor_configs_file:s0"
 
+# Fix MIDAS model detection
+sed -i "s/a73xq/a72q/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+
 # Upgrade singletake blobs
 DELETE_FROM_WORK_DIR "vendor" "etc/singletake"
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/singletake" 0 2000 755 "u:object_r:vendor_configs_file:s0"
