7565bf244d464ede965cf7465a757ffc25263077
Rafael Alvarado Navarro <ealvaradobucio26@gmail.com>
2025-11-25 23:45:54 -0600

    Remove libDocDeblur.enhanceX from customize.sh

--- target/dm2q/patches/camera/customize.sh
+++ target/dm2q/patches/camera/customize.sh
@@ -12,7 +12,6 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution_raw
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libDocObjectRemoval.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libDocDeblur.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libDocDeblur.enhanceX.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libDocObjectRemoval.enhanceX.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libDLInterface_aidl.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
