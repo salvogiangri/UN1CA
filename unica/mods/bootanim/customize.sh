bd989b02961da811c9775eeba200cecdd83ee811
=?UTF-8?q?=E9=84=AD=E7=A7=89=E6=AD=A3?= <126307842+mattchengg@users.noreply.github.com>
2025-11-29 16:34:47 +0100

    unica: mods: bootanim: determine device resolution from camera feature
    Change-Id: If8731086ac7eefa3b5728c9f70f83ec91c0caf92

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,16 +1,18 @@
-# 1080x2340 devices
-TWOTHREE_TARGETS="dm1q dm2q g0q r0q"
-# 1080x2400 devices
-TWOFOUR_TARGETS="a52q a52xq a52sxq a71 a72q a73xq m52xq r8q r9q r9q2"
+XML_FILE="$WORK_DIR/system/system/cameradata/camera-feature.xml"
+if [ ! -f "$XML_FILE" ]; then
+    ABORT "File not found: ${WORK_DIR//$SRC_DIR\//}/system/system/cameradata/camera-feature.xml"
+fi
 
-if grep -q -w "$TARGET_CODENAME" <<< "$TWOTHREE_TARGETS" ; then
-    LOG "- Adding 2024 boot animation blobs"
-    cp -a "$MODPATH/1080x2340/"* "$WORK_DIR/system/system/media"
-elif grep -q -w "$TARGET_CODENAME" <<< "$TWOFOUR_TARGETS"; then
-    LOG "- Adding 2024 boot animation blobs"
-    cp -a "$MODPATH/1080x2400/"* "$WORK_DIR/system/system/media"
+if grep -q "_CAMCORDER_RESOLUTION_FEATURE_MAP_2336X1080" "$XML_FILE" && \
+        ! $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
+    LOG "- Adding 2024 boot animation blobs (1080x2340)"
+    EVAL "cp -a \"$MODPATH/1080x2340/\"* \"$WORK_DIR/system/system/media\""
+elif grep -q "_CAMCORDER_RESOLUTION_FEATURE_MAP_2400X1080" "$XML_FILE" && \
+        ! $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
+    LOG "- Adding 2024 boot animation blobs (1080x2400)"
+    EVAL "cp -a \"$MODPATH/1080x2400/\"* \"$WORK_DIR/system/system/media\""
 else
     LOGW "Unknown boot animation resolution for \"$TARGET_CODENAME\". Skipping"
 fi
 
-unset TWOTHREE_TARGETS TWOFOUR_TARGETS
+unset XML_FILE
