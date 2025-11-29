60ca45509efa9fcb9085d818bd7ac056e0406a36
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-11-29 22:49:20 +0100

    sm7125: patches: add required camera libs
    Change-Id: I70c6be0ce1bd41062a9270b9cf8cab693d8511bd
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- platform/sm7125/patches/camera/customize.sh
+++ platform/sm7125/patches/camera/customize.sh
@@ -11,3 +11,16 @@ LOG_STEP_IN "- Replace MIDAS config files with a73xqxx"
 DELETE_FROM_WORK_DIR "vendor" "etc/midas"
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/midas" 0 2000 755 "u:object_r:vendor_configs_file:s0"
 LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding camera libs from stock"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSceneDetector_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/liblow_light_hdr.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_dynamic_range.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhumantracking.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhumantracking_util.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libveengine.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding camera lib from a73xqxx"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libsecimaging_pdk.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
