cae062f6c882fb506f5b03a0c8159766c7baf33d
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-11-30 07:50:21 +0000

    platforms: sm7125: init
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Change-Id: Ibc77569c3611fdba855965b64864190116501a25
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

---
+++ platform/sm7125/debloat.sh
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Debloat list for Qualcomm Snapdragon 720G devices (sm7125)
+# - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
+# - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
+# - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
+
+# Overlays
+SYSTEM_DEBLOAT+="
+system/app/WifiRROverlayAppLls
+"
+
+# mAFPC
+SYSTEM_DEBLOAT+="
+system/bin/mafpc_write
+"
+
+# HDCP
+SYSTEM_DEBLOAT+="
+system/bin/dhkprov
+system/bin/qchdcpkprov
+system/etc/init/dhkprov.rc
+system/lib64/vendor.samsung.hardware.security.hdcp.keyprovisioning@1.0.so
+"
+
+# GameDriver
+SYSTEM_DEBLOAT+="
+system/priv-app/GameDriver-SM8450
+"
+
+# system_ext clean-up
+SYSTEM_EXT_DEBLOAT+="
+etc/permissions/com.qti.location.sdk.xml
+etc/permissions/com.qualcomm.location.xml
+etc/permissions/privapp-permissions-com.qualcomm.location.xml
+framework/com.qti.location.sdk.jar
+priv-app/com.qualcomm.location
+"
---
+++ platform/sm7125/patches/camera/customize.sh
@@ -0,0 +1,12 @@
+LOG_STEP_IN "- Adding ImageTagger lib from a73xqxx"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding Polarr libs from a73xqxx"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/etc/public.libraries-polarr.txt" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libBestComposition.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libFeature.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libPolarrSnap.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libTracking.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
