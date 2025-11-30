6bd73b933be88023db4b46071e1374eff5baa5d8
Majaahh <maja.blaszczak@proton.me>
2025-11-30 15:38:14 +0100

    unica: patches: adb: add support for essi
    Change-Id: I5642d7717d0e8ba447c433287028bcd27cc76f72
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- unica/patches/adb/customize.sh
+++ unica/patches/adb/customize.sh
@@ -25,18 +25,20 @@ SET_PROP "system" "ro.logd.kernel" "true"
 # Do not filter out Samsung processes in logs
 SET_PROP_IF_DIFF "system" "persist.log.semlevel" "0xFFFFFFFF"
 
-if [[ "$TARGET_OS_SINGLE_SYSTEM_IMAGE" == "mssi" ]]; then
-    RC_FILE="init.project.rc"
+if [[ "$TARGET_OS_SINGLE_SYSTEM_IMAGE" == "essi" ]]; then
+    RC_FILE="init.$(GET_PROP "vendor" "ro.product.board").rc"
+elif [[ "$TARGET_OS_SINGLE_SYSTEM_IMAGE" == "mssi" ]]; then
+    RC_FILE="hw/init.project.rc"
 elif [[ "$TARGET_OS_SINGLE_SYSTEM_IMAGE" == "qssi" ]]; then
-    RC_FILE="init.target.rc"
+    RC_FILE="hw/init.target.rc"
 fi
-if [ "$RC_FILE" ] && [ -f "$WORK_DIR/vendor/etc/init/hw/$RC_FILE" ]; then
-    if ! grep -q "persist.vendor.radio.port_index" "$WORK_DIR/vendor/etc/init/hw/$RC_FILE"; then
+if [ "$RC_FILE" ] && [ -f "$WORK_DIR/vendor/etc/init/$RC_FILE" ]; then
+    if ! grep -q "persist.vendor.radio.port_index" "$WORK_DIR/vendor/etc/init/$RC_FILE"; then
         {
             echo ""
             echo "on property:persist.vendor.radio.port_index=\"\""
             echo "    setprop sys.usb.config adb"
-        } >> "$WORK_DIR/vendor/etc/init/hw/$RC_FILE"
+        } >> "$WORK_DIR/vendor/etc/init/$RC_FILE"
     fi
     unset RC_FILE
 fi
