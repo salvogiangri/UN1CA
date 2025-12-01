0f7cba9866d96f0395989a09e57ae6a6e84f1698
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-12-01 07:13:07 +0000

    target: a52q: init
    Co-authored-by: TestaMic <mateidiac9@gmail.com>
    Change-Id: I0542b3602eae89a136e950ce17fab8b57cef8484
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

---
+++ target/a52q/config.sh
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A52 (a52q)
+TARGET_NAME="Galaxy A52"
+TARGET_CODENAME="a52q"
+TARGET_PLATFORM="sm7125"
+TARGET_FIRMWARE="SM-A525F/SER/352938771234569"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_PLATFORM_SDK_VERSION=34
+TARGET_PRODUCT_SHIPPING_API_LEVEL=30
+
+# SEC Product Feature
+TARGET_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME="dvfs_policy_sm7125_xx"
+TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME="siop_a52q_sm7125"
