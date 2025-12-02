be3b6496f575fae1a882ac67981f01812a095063
Rafael Alvarado Navarro <ealvaradobucio26@gmail.com>
2025-12-02 17:46:45 -0600

    Update customize.sh

--- platform/sm8550/patches/camera/customize.sh
+++ platform/sm8550/patches/camera/customize.sh
@@ -1,3 +1,6 @@
+TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
+
+
 # Add ImageTagger lib
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
@@ -9,3 +12,24 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhumantracking_util.
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsaiv_HprFace_cmh_support_jni.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsecimaging_pdk.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libveengine.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Replace midas blobs
+DELETE_FROM_WORK_DIR "vendor" "etc/midas"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "vendor" "etc/midas" 0 2000 755 "u:object_r:vendor_configs_file:s0"
+
+# Replace singletake blobs
+DELETE_FROM_WORK_DIR "vendor" "etc/singletake"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "vendor" "etc/singletake" 0 2000 755 "u:object_r:vendor_configs_file:s0"
+
+# Patch portrait data to fix AI photo editor
+LOG_STEP_IN "- Fix AI photo editor"
+cp -a --preserve=all \
+    "$TARGET_FIRMWARE_PATH/system/system/cameradata/portrait_data/single_bokeh_feature.json" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/unica_bokeh_feature.json"
+SET_METADATA "system" "system/cameradata/portrait_data/unica_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
+sed -i "s/MODEL_TYPE_INSTANCE_CAPTURE/MODEL_TYPE_OBJ_INSTANCE_CAPTURE/g" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json"
+sed -i \
+    's/system\/cameradata\/portrait_data\/single_bokeh_feature.json/system\/cameradata\/portrait_data\/unica_bokeh_feature.json\x00/g' \
+    "$WORK_DIR/system/system/lib64/libPortraitSolution.camera.samsung.so"
+LOG_STEP_OUT
