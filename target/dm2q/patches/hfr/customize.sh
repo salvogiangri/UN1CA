a3b28b9ed7e09a41a593661fd4cd3dacfafb0c3a
Rafael Alvarado Navarro <ealvaradobucio26@gmail.com>
2025-12-08 02:43:29 -0600

    Remove Surface Flinger properties for frame rate
    Removed several Surface Flinger properties related to frame rate and phase offsets.

--- target/dm2q/patches/hfr/customize.sh
+++ target/dm2q/patches/hfr/customize.sh
@@ -10,21 +10,5 @@ EVAL "sed -i \"/use_content_detection/a ro.surface_flinger.set_idle_timer_ms=$ID
 LOG "- Adding \"ro.surface_flinger.set_touch_timer_ms\" prop with \"$TOUCH_TIMER_MS\" in /vendor/build.prop"
 EVAL "sed -i \"/set_idle_timer_ms/a ro.surface_flinger.set_touch_timer_ms=$TOUCH_TIMER_MS\" \"$WORK_DIR/vendor/build.prop\""
 SET_PROP "vendor" "ro.surface_flinger.enable_frame_rate_override" "true"
-SET_PROP "vendor" "ro.surface_flinger.game_default_frame_rate_override" "60"
-
-# Surface Flinger phase offset properties for high refresh rate
-SET_PROP "vendor" "debug.sf.enable_advanced_sf_phase_offset" "0"
-SET_PROP "vendor" "debug.sf.use_phase_offsets_as_durations" "0"
-SET_PROP "vendor" "debug.sf.early_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.early_app_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.early_gl_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.early_gl_app_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.high_fps_early_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.high_fps_early_gl_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.high_fps_late_app_phase_offset_ns" "100000"
-SET_PROP "vendor" "debug.sf.high_fps_late_sf_phase_offset_ns" "100000"
-
-# Debug refresh rate overlay
-SET_PROP "vendor" "debug.sf.show_refresh_rate_overlay_render_rate" "true"
 
 unset IDLE_TIMER_MS TOUCH_TIMER_MS
