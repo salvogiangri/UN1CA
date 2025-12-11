76c17916abff7266d5a026371e14733e6fbde216
Saad <saadelasfur@gmail.com>
2025-12-11 22:35:52 +0300

    unica: mods: bootanim: determine device resolution from target boot animation blobs
    Signed-off-by: Saad <saadelasfur@gmail.com>

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,18 +1,14 @@
-XML_FILE="$WORK_DIR/system/system/cameradata/camera-feature.xml"
-if [ ! -f "$XML_FILE" ]; then
-    ABORT "File not found: ${WORK_DIR//$SRC_DIR\//}/system/system/cameradata/camera-feature.xml"
-fi
+TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
+TARGET_SCREEN_HEIGHT="$(printf "%d" "0x$(READ_BYTES_AT "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/media/bootsamsung.qmg" "8" "2")")"
 
-if grep -q "_CAMCORDER_RESOLUTION_FEATURE_MAP_2336X1080" "$XML_FILE" && \
-        ! $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
+if [[ "$TARGET_SCREEN_HEIGHT" == "2340" ]]; then
     LOG "- Adding 2024 boot animation blobs (1080x2340)"
     EVAL "cp -a \"$MODPATH/1080x2340/\"* \"$WORK_DIR/system/system/media\""
-elif grep -q "_CAMCORDER_RESOLUTION_FEATURE_MAP_2400X1080" "$XML_FILE" && \
-        ! $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
+elif [[ "$TARGET_SCREEN_HEIGHT" == "2400" ]]; then
     LOG "- Adding 2024 boot animation blobs (1080x2400)"
     EVAL "cp -a \"$MODPATH/1080x2400/\"* \"$WORK_DIR/system/system/media\""
 else
     LOGW "Unknown boot animation resolution for \"$TARGET_CODENAME\". Skipping"
 fi
 
-unset XML_FILE
+unset TARGET_FIRMWARE_PATH TARGET_SCREEN_HEIGHT
