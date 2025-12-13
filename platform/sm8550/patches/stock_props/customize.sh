fb177227003eb6f6355c1143a51b5b358a51bbdc
Rafael Alvarado Navarro <ealvaradobucio26@gmail.com>
2025-12-13 00:31:48 -0600

    Add ro.netflix.bsp_rev property to build.prop

--- platform/sm8550/patches/stock_props/customize.sh
+++ platform/sm8550/patches/stock_props/customize.sh
@@ -1,2 +1,4 @@
 LOG "- Adding \"ro.netflix.bsp_rev\" prop with \"Q8550-36432-1\" in /system/system/build.prop"
 EVAL "sed -i \"/ro.smps.gain.spk/i ro.netflix.bsp_rev=Q8550-36432-1\" \"$WORK_DIR/system/system/build.prop\""
+
+SET_PROP "vendor" "ro.product.vendor.device" "${TARGET_CODENAME}xxx"
