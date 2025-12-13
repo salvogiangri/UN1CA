d16e3aca7b169f3853f18bbdd3035eeda3fe5129
Saad <saadelasfur@gmail.com>
2025-12-13 13:13:36 +0300

    r9q/r9q2: patches: camera: add missing stock libs
    Signed-off-by: Saad <saadelasfur@gmail.com>

--- target/r9q2/patches/camera/customize.sh
+++ target/r9q2/patches/camera/customize.sh
@@ -1,2 +1,14 @@
-# Add ImageTagger blobs
+# Add ImageTagger lib
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Add stock camera libs
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_res.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSceneDetector_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution_wrapper_v2.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-arcsoft.txt"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-camera.samsung.txt"
+{
+    echo "libLttEngine.camera.samsung.so"
+} >> "$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt"
