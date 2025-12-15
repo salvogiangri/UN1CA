e011468e92aca870b2b7d3ad68e481f0b1227317
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-12-15 14:37:18 +0100

    exynos2100: camera: clean up
    Change-Id: I4470e3211cc20d41f8548a9d0bf7feb5046cb5f9

--- platform/exynos2100/patches/camera/customize.sh
+++ platform/exynos2100/patches/camera/customize.sh
@@ -1,67 +1,64 @@
-LOG_STEP_IN "- Adding stock camera libs"
-BLOBS_LIST="
-system/lib64/libenn_wrapper_system.so
-system/lib64/libdualcam_portraitlighting_gallery_360.so
-"
-for blob in $BLOBS_LIST
-do
-    DELETE_FROM_WORK_DIR "system" "$blob"
-done
+# Add ImageTagger lib
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
-BLOBS_LIST="
-system/lib64/libdualcam_portraitlighting_gallery_360_lite.so
-system/lib64/libdualcam_refocus_gallery_50.so
-system/lib64/libFood.camera.samsung.so
-system/lib64/libFoodDetector.camera.samsung.so
-system/lib64/libeden_wrapper_system.so
-system/lib64/libhigh_dynamic_range.arcsoft.so
-system/lib64/libhigh_res.arcsoft.so
-system/lib64/libHpr_RecFace_dl_v1.0.camera.samsung.so
-system/lib64/libhumantracking.arcsoft.so
-system/lib64/libImageTagger.camera.samsung.so
-system/lib64/liblow_light_hdr.arcsoft.so
-system/lib64/libMultiFrameProcessing30.camera.samsung.so
-system/lib64/libMultiFrameProcessing30Tuning.camera.samsung.so
-system/lib64/libPortraitDistortionCorrectionCali.arcsoft.so
-system/lib64/libsecuresnap_aidl.snap.samsung.so
-system/lib64/libsnap_aidl.snap.samsung.so
-system/lib64/libsuperresolution_wrapper_v2.camera.samsung.so
-system/lib64/libsuperresolution.arcsoft.so
-system/lib64/libSwIsp_core.camera.samsung.so
-system/lib64/libSwIsp_wrapper_v1.camera.samsung.so
-system/lib64/libsaiv_HprFace_cmh_support_jni.camera.samsung.so
-"
+# Add Polarr libs
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-polarr.txt" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libPolarrSnap.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Add 360 lite libs
+DELETE_FROM_WORK_DIR "system" "system/lib64/libdualcam_portraitlighting_gallery_360.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlighting_gallery_360_lite.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Add Snap libs
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-snap.samsung.txt" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libeden_wrapper_system.so" 0 0 644 "u:object_r:system_lib_file:s0"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libenn_wrapper_system.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsecuresnap_aidl.snap.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsnap_aidl.snap.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# Add camera libs
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/public.libraries-arcsoft.txt" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libFace_Landmark_Engine.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libFacialStickerEngine.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libFood.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+EVAL "echo \"libFood.camera.samsung.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt\""
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libFoodDetector.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+EVAL "echo \"libFoodDetector.camera.samsung.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt\""
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libHpr_RecFace_dl_v1.0.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libLocalTM_pcc.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libMultiFrameProcessing30.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libMultiFrameProcessing30Tuning.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_core.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libfacialrestoration.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_dynamic_range.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_res.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhumantracking.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhumantracking_util.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libimage_enhancement.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsaiv_HprFace_cmh_support_jni.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 if [[ "$TARGET_CODENAME" == "p3s" ]]; then
-    BLOBS_LIST+="
-    system/lib64/libsuperresolution_raw.arcsoft.so
-    system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so
-    system/lib64/libuwsuperresolution.arcsoft.so
-    system/lib64/libuwsuperresolution_wrapper_v1.camera.samsung.so
-    "
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsame_source_hdr.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+fi
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsecimaging_pdk.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution_wrapper_v2.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+EVAL "echo \"libsuperresolution_wrapper_v2.camera.samsung.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt\""
+if [[ "$TARGET_CODENAME" == "p3s" ]]; then
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolution_raw.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    EVAL "echo \"libsuperresolutionraw_wrapper_v2.camera.samsung.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt\""
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libuwsuperresolution.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libuwsuperresolution_wrapper_v1.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    EVAL "echo \"libuwsuperresolution_wrapper_v1.camera.samsung.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt\""
+fi
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libveengine.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "36" ]; then
+    # Upgrade midas blobs
+    ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/midas/midas_config.json" 0 0 644 "u:object_r:vendor_configs_file:s0"
 fi
 
-for blob in $BLOBS_LIST
-do
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0"
-done
-LOG_STEP_OUT
-
-LOG_STEP_IN "- Adding S21 FE (r9sxxx) MIDAS blobs"
-DELETE_FROM_WORK_DIR "vendor" "etc/midas"
-DELETE_FROM_WORK_DIR "vendor" "etc/VslMesDetector"
-ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/midas"
-ADD_TO_WORK_DIR "r9sxxx" "vendor" "etc/VslMesDetector"
-LOG_STEP_OUT
-
 LOG "- Fixing MIDAS model detection"
-sed -i "s/r9s/r0s/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
-
-LOG_STEP_IN "- Adding S21 FE (r9sxxx) Photo Remaster Service"
-DELETE_FROM_WORK_DIR "system" "system/priv-app/PhotoRemasterService/oat"
-ADD_TO_WORK_DIR "r9sxxx" "system" "system/priv-app/PhotoRemasterService/PhotoRemasterService.apk"
-LOG_STEP_OUT
-
-LOG_STEP_IN "- Adding S21 FE (r9sxxx) MIDAS libs"
-ADD_TO_WORK_DIR "r9sxxx" "system" "system/lib64/libmidas_core.camera.samsung.so"
-ADD_TO_WORK_DIR "r9sxxx" "system" "system/lib64/libmidas_DNNInterface.camera.samsung.so"
-LOG_STEP_OUT
+EVAL "sed -i \"s/$TARGET_CODENAME/r0s/g\" \"$WORK_DIR/vendor/etc/midas/midas_config.json\""
