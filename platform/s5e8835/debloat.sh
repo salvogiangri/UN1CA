dea6504427d9fe5325bf3dffc897db157ebb0122
mattchengg <mattcheng20080205@gmail.com>
2025-12-15 13:01:03 +0800

    platform: s5e8835: init

---
+++ platform/s5e8835/debloat.sh
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Debloat list for Samsung Exynos 1380 devices (s5e8835)
+# - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
+# - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
+# - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
---
+++ platform/s5e8835/sff.sh
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2025 mattchengg
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# SEC Floating Feature configuration file for Samsung Exynos 1380 devices (s5e8835)
+
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
