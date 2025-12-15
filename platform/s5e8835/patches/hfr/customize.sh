52317ca4e9e4857f594d04795ff2fa442a1d7516
salvogiangri <salvogiangri@users.noreply.github.com>
2025-12-15 13:01:09 +0800

    platform: s5e8835: patches: hfr: init

---
+++ platform/s5e8835/patches/hfr/customize.sh
@@ -0,0 +1,15 @@
+# https://android.googlesource.com/platform/frameworks/native/+/refs/tags/android-16.0.0_r2/services/surfaceflinger/Scheduler/RefreshRateSelector.h#314
+IDLE_TIMER_MS=250
+# https://android.googlesource.com/platform/frameworks/native/+/refs/tags/android-16.0.0_r2/services/surfaceflinger/sysprop/SurfaceFlingerProperties.sysprop#346
+TOUCH_TIMER_MS=300
+
+SET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate" "true"
+LOG "- Adding \"ro.surface_flinger.set_idle_timer_ms\" prop with \"$IDLE_TIMER_MS\" in /vendor/build.prop"
+EVAL "sed -i \"/use_content_detection/a ro.surface_flinger.set_idle_timer_ms=$IDLE_TIMER_MS\" \"$WORK_DIR/vendor/build.prop\""
+LOG "- Adding \"ro.surface_flinger.set_touch_timer_ms\" prop with \"$TOUCH_TIMER_MS\" in /vendor/build.prop"
+EVAL "sed -i \"/set_idle_timer_ms/a ro.surface_flinger.set_touch_timer_ms=$TOUCH_TIMER_MS\" \"$WORK_DIR/vendor/build.prop\""
+LOG "- Replacing \"ro.surface_flinger.enable_frame_rate_override\" prop with \"true\" in /vendor/build.prop"
+EVAL "sed -i \"/enable_frame_rate_override/d\" \"$WORK_DIR/vendor/build.prop\""
+EVAL "sed -i \"/persist.sys.usb.config/i ro.surface_flinger.enable_frame_rate_override=true\" \"$WORK_DIR/vendor/build.prop\""
+
+unset IDLE_TIMER_MS TOUCH_TIMER_MS
