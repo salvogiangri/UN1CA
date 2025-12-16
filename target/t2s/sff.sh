70a4e0989baffc31ca473487a74b568ee915f774
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-12-16 23:38:11 +0100

    palette: floating_feature: enable AOD live clock
    Change-Id: I6b0dce1512c86daa3662e07a95345456d8c5da48

--- target/t2s/sff.sh
+++ target/t2s/sff.sh
@@ -17,6 +17,9 @@
 
 # SEC Floating Feature configuration file for Galaxy S21+ 5G (Exynos) (t2s)
 
+# Enable AOD live clock
+SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM=activeclock=7,aodversion=7,clocktransition,coverboldfont
+
 # Rainbow LED Cover level
 SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_NFC_LED_COVER_LEVEL=100
 
