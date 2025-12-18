d38d5487116f85ae47141cd626bffcd756f5d685
Majaahh <maja.blaszczak@proton.me>
2025-12-18 16:24:42 +0100

    a53x: kernel: init
    Change-Id: I593963350554a45518527e27ea281f7d03cf1659
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a53x/patches/kernel/customize.sh
@@ -0,0 +1,54 @@
+# [
+GET_URL()
+{
+    _CHECK_NON_EMPTY_PARAM "ASSET" "$1" || return 1
+
+    local KERNEL_URL="https://api.github.com/repos/UN1CA/kernel_samsung_s5e8825/releases/latest"
+
+    curl -s "$KERNEL_URL" | jq -r --arg i "$1" '.assets[] | select(.name | test($i)) | .browser_download_url' | head -n 1
+}
+# ]
+
+KERNEL_ARCHIVE_URL="$(GET_URL "^UN1CA_Kernel-.*-a53x\.tar$")"
+DTBO_ARCHIVE_URL="$(GET_URL "^UN1CA_DTBO-.*-a53x\.tar$")"
+
+if [ -d "$TMP_DIR" ]; then
+    EVAL "rm -rf \"$TMP_DIR\""
+fi
+EVAL "mkdir -p \"$TMP_DIR\""
+
+DOWNLOAD_FILE "$KERNEL_ARCHIVE_URL" "$TMP_DIR/$(basename "$KERNEL_ARCHIVE_URL")" &
+DOWNLOAD_FILE "$DTBO_ARCHIVE_URL" "$TMP_DIR/$(basename "$DTBO_ARCHIVE_URL")" &
+
+# shellcheck disable=SC2046
+wait $(jobs -p) || return 1
+
+while IFS= read -r f; do
+    TAR="$(basename "$f")"
+
+    LOG "- Extracting $TAR"
+    EVAL "tar -xvf \"$TMP_DIR/$TAR\" -C \"$TMP_DIR\""
+    EVAL "rm -f \"$TMP_DIR/$TAR\""
+
+    unset TAR
+done < <(find "$TMP_DIR" -maxdepth 1 -type f -name "*.tar")
+
+while IFS= read -r f; do
+    IMG="$(basename "${f%.lz4}")"
+
+    LOG "- Extracting $IMG.lz4"
+    EVAL "lz4 -df --rm \"$TMP_DIR/$IMG.lz4\" \"$TMP_DIR/$IMG\""
+
+    LOG "- Replacing $IMG"
+    if [ -f "$WORK_DIR/kernel/$IMG" ]; then
+        EVAL "rm -f \"$WORK_DIR/kernel/$IMG\""
+    fi
+    EVAL "mv \"$TMP_DIR/$IMG\" \"$WORK_DIR/kernel/$IMG\""
+
+    unset IMG
+done < <(find "$TMP_DIR" -maxdepth 1 -type f -name "*.img.lz4")
+
+EVAL "rm -rf \"$TMP_DIR\""
+
+unset DTBO_ARCHIVE_URL KERNEL_ARCHIVE_URL
+unset -f GET_URL
