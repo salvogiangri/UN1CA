816c4da2cd489879c3c11103b55dc54835d77704
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-12-18 12:04:48 +0100

    unica: mods: bootanim: minor adjustments
    Change-Id: I7563d520eb293cda4debda1bfdb190c38b618d6a

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,14 +1,14 @@
 TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
-TARGET_SCREEN_HEIGHT="$(printf "%d" "0x$(READ_BYTES_AT "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/media/bootsamsung.qmg" "8" "2")")"
 
-if [[ "$TARGET_SCREEN_HEIGHT" == "2340" ]]; then
-    LOG "- Adding 2024 boot animation blobs (1080x2340)"
-    EVAL "cp -a \"$MODPATH/1080x2340/\"* \"$WORK_DIR/system/system/media\""
-elif [[ "$TARGET_SCREEN_HEIGHT" == "2400" ]]; then
-    LOG "- Adding 2024 boot animation blobs (1080x2400)"
-    EVAL "cp -a \"$MODPATH/1080x2400/\"* \"$WORK_DIR/system/system/media\""
+TARGET_SCREEN_RESOLUTION="$(printf "%d" "0x$(READ_BYTES_AT "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/media/bootsamsung.qmg" "6" "2")")"
+TARGET_SCREEN_RESOLUTION+="x"
+TARGET_SCREEN_RESOLUTION+="$(printf "%d" "0x$(READ_BYTES_AT "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/media/bootsamsung.qmg" "8" "2")")"
+
+if [ -d "$MODPATH/$TARGET_SCREEN_RESOLUTION" ]; then
+    LOG "- Adding 2024 boot animation blobs ($TARGET_SCREEN_RESOLUTION)"
+    EVAL "cp -a \"$MODPATH/$TARGET_SCREEN_RESOLUTION/\"* \"$WORK_DIR/system/system/media\""
 else
-    LOGW "Unknown boot animation resolution for \"$TARGET_CODENAME\". Skipping"
+    LOGW "No boot animation blobs available for $TARGET_SCREEN_RESOLUTION resolution. Skipping"
 fi
 
-unset TARGET_FIRMWARE_PATH TARGET_SCREEN_HEIGHT
+unset TARGET_FIRMWARE_PATH TARGET_SCREEN_RESOLUTION
