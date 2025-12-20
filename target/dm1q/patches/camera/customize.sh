db5cc626a976250e71bfa33329abb174fcea53fc
Ferdinand Saurenbach <ferdinand@fsrb.de>
2025-12-20 10:35:36 +0100

    target: dmxq: patches: camera: sort alphabetically
    Change-Id: If9bd0c64ec347ee04245369235eda0821bfd3c9c
    Signed-off-by: Ferdinand Saurenbach <ferdinand@fsrb.de>

--- target/dm1q/patches/camera/customize.sh
+++ target/dm1q/patches/camera/customize.sh
@@ -2,12 +2,12 @@ echo "Add stock camera libs"
 BLOBS_LIST="
 system/etc/public.libraries-arcsoft.txt
 system/etc/public.libraries-camera.samsung.txt
+system/lib64/libSwIsp_core.camera.samsung.so
+system/lib64/libSwIsp_wrapper_v1.camera.samsung.so
 system/lib64/libaiclearzoom_raw.arcsoft.so
 system/lib64/libaiclearzoomraw_wrapper_v1.camera.samsung.so
 system/lib64/libsuperresolution_raw.arcsoft.so
 system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so
-system/lib64/libSwIsp_core.camera.samsung.so
-system/lib64/libSwIsp_wrapper_v1.camera.samsung.so
 "
 for blob in $BLOBS_LIST
 do
--- target/dm2q/patches/camera/customize.sh
+++ target/dm2q/patches/camera/customize.sh
@@ -2,14 +2,14 @@ echo "Add stock camera libs"
 BLOBS_LIST="
 system/etc/public.libraries-arcsoft.txt
 system/etc/public.libraries-camera.samsung.txt
+system/lib64/libDLInterface_aidl.camera.samsung.so
+system/lib64/libDocDeblur.camera.samsung.so
+system/lib64/libDocObjectRemoval.camera.samsung.so
+system/lib64/libDocObjectRemoval.enhanceX.samsung.so
 system/lib64/libaiclearzoom_raw.arcsoft.so
 system/lib64/libaiclearzoomraw_wrapper_v1.camera.samsung.so
 system/lib64/libsuperresolution_raw.arcsoft.so
 system/lib64/libsuperresolutionraw_wrapper_v2.camera.samsung.so
-system/lib64/libDocObjectRemoval.camera.samsung.so
-system/lib64/libDocDeblur.camera.samsung.so
-system/lib64/libDocObjectRemoval.enhanceX.samsung.so
-system/lib64/libDLInterface_aidl.camera.samsung.so
 "
 for blob in $BLOBS_LIST
 do
