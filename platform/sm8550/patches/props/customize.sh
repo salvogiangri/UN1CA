c20f3edce8f08cfadd990b8d71bde1b539fc6af3
CyberK1911 <153315683+CyberK1911@users.noreply.github.com>
2025-12-21 00:50:20 +0530

    target: dmxq: only apply display config fix to required devices (#5)

--- platform/sm8550/patches/props/customize.sh
+++
@@ -1,3 +0,0 @@
-# Spoof device name (Some users have reported that their display config looked weird permanently after installing a random magisk module / OneUI 7 Beta)
-# This does not seem to cause any other problems
-SET_PROP "vendor" "ro.product.vendor.device" "${TARGET_CODENAME}xxx"
