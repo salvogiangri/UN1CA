ec9daa0a8bf428d1f0567fac84ef571deccdb667
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-01-01 21:39:27 +0800

    sm7125: display: fix `GetCustomDimensions()` error
    Fix `GetCustomDimensions()` error logspam from graphics mapper caused by
    early exit after calling `getMetaData()` especially when using newer
    adreno drivers.
    
    No functional change.
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- platform/sm7125/patches/display/customize.sh
+++ platform/sm7125/patches/display/customize.sh
@@ -61,4 +61,16 @@ ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.graphics.mapper@3.
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.graphics.mapper@4.0-impl-qti-display.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
 
 HEX_PATCH "$WORK_DIR/vendor/lib64/libsdmutils.so" "40F9F303012A3401" "40F9130080523401"
+
+# Workaround getMetaData() return path to fix GetCustomDimensions() error (from r9q).
+# Un-inline pixel format checks from:
+# if (format != HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS_UBWC || format != HAL_PIXEL_FORMAT_YCbCr_420_TP10_UBWC ||
+#      format != HAL_PIXEL_FORMAT_YCbCr_420_P010_UBWC)
+# to:
+# if (!IsUBwcFormat())
+# to retain padding and file size
+HEX_PATCH "$WORK_DIR/vendor/lib64/libgrallocutils.so" "60040035a8c35eb828040034a82e40b9" "e803002ae0031f2a28040035a8c35eb8"
+HEX_PATCH "$WORK_DIR/vendor/lib64/libgrallocutils.so" "1f910471200100542981815269f4af72" "e8030034a82e40b9e003082a75feff97"
+HEX_PATCH "$WORK_DIR/vendor/lib64/libgrallocutils.so" "1f01096ba0000054c980815269f4af72" "e803002ae0031f2a280300341f2003d5"
+HEX_PATCH "$WORK_DIR/vendor/lib64/libgrallocutils.so" "1f01096bc1020054bf431ef8a9aa4329" "1f2003d51f2003d5bf431ef8a9aa4329"
 LOG_STEP_OUT
