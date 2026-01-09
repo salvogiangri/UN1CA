0bd2b431730bc234169e001fb06dc3a1d9ac63b4
Trần Phương ジ <luuphuongtran0520@gmail.com>
2026-01-10 00:35:29 +0700

    Delete target/b0q directory

--- target/b0q/patches/stock_blobs/customize.sh
+++
@@ -1,2 +0,0 @@
-echo "Add stock WifiRROverlayAppLargeTcpBuffer overlay"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/app/WifiRROverlayAppLargeTcpBuffer/WifiRROverlayAppLargeTcpBuffer.apk" 0 0 644 "u:object_r:system_file:s0"
