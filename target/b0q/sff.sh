79ea879e2fa8fc5402676deed5043b676812e4e0
Trần Phương ジ <luuphuongtran0520@gmail.com>
2026-01-10 00:44:35 +0700

    Update sff.sh

--- target/b0q/sff.sh
+++ target/b0q/sff.sh
@@ -17,18 +17,30 @@
 
 # SEC Floating Feature configuration file for Galaxy S22 Ultra (b0q)
 
+# Camera
 SEC_FLOATING_FEATURE_CAMERA_CONFIG_CAMID_TELE2=54
 SEC_FLOATING_FEATURE_CAMERA_CONFIG_NIGHT_FRONT_DISPLAY_FLASH_TRANSPARENT=65
 SEC_FLOATING_FEATURE_CAMERA_CONFIG_UW_DISTORTION_CORRECTION=0,109,2,19010102,4000,3000,0,0,60,0
 SEC_FLOATING_FEATURE_CAMERA_CONFIG_VENDOR_LIB_INFO=localtm.samsung.v1_1,super_resolution_raw.arcsoft.v1,beauty.samsung.v4,face_landmark.arcsoft.v2_1,food.samsung.v1,facial_attribute.samsung.v1,human_tracking_hand.arcsoft.v4,facial_restoration.arcsoft.v1,fr_tracking.arcsoft.v1,smart_scan.samsung.v2,aimode.samsung.v1,mfhdr.arcsoft.v4,llhdr.arcsoft.v4,aebhdr.arcsoft.v1,hybridhdr.arcsoft.v1,single_bokeh.samsung.v2,dual_bokeh.samsung.v1_1,super_night.mpi.v2,swuwdc.arcsoft.v1,image_codec.samsung.v1,uw_super_resolution.arcsoft.v1,high_res.arcsoft.v1,fusion_high_res.arcsoft.v1,image_enhance.arcsoft.v1,event_detection.samsung.v2,selfie_correction.samsung.v1,pro_single_rgb.mpi.v1
+
+# S Pen
 SEC_FLOATING_FEATURE_COMMON_CONFIG_BLE_SPEN_SPEC=builtin,button,airmotion
-SEC_FLOATING_FEATURE_COMMON_CONFIG_DYN_RESOLUTION_CONTROL=WQHD,FHD,HD
 SEC_FLOATING_FEATURE_COMMON_SUPPORT_BLE_SPEN=TRUE
 SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_SPEN_GARAGE_SPEC=type=insert, bundled=true, unbundled_spec=remote
 SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_SPEN_VERSION=70
+
+# Display
+SEC_FLOATING_FEATURE_COMMON_CONFIG_DYN_RESOLUTION_CONTROL=WQHD,FHD,HD
 SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=3
+
+# Frameworks
+SEC_FLOATING_FEATURE_SYSTEM_CONFIG_SIOP_POLICY_FILENAME=siop_b0q_sm8450
+
+# Lockscreen
 SEC_FLOATING_FEATURE_LOCKSCREEN_CONFIG_PUNCHHOLE_VI=face,pos:0.5:0.0196,size:0.0833:0.0388,type:circle
+
+# Device specific
 SEC_FLOATING_FEATURE_SETTINGS_CONFIG_BRAND_NAME=Galaxy S22 Ultra
 SEC_FLOATING_FEATURE_SETTINGS_CONFIG_ELECTRIC_RATED_VALUE=DC 10 V; 4.5 A
 SEC_FLOATING_FEATURE_SETTINGS_CONFIG_FCC_ID=A3LSMS908E
-SEC_FLOATING_FEATURE_SYSTEM_CONFIG_SIOP_POLICY_FILENAME=siop_b0q_sm8450
+
