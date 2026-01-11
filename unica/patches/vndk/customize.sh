ceb03522853a015eeeb1e22ff726d2485ce34241
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-01-11 18:51:09 +0100

    unica: patches: vndk: add VNDK 34 apex

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -18,6 +18,9 @@ ADD_TARGET_VNDK_APEX() {
         "33")
             ADD_TO_WORK_DIR "dm1qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
+        "34")
+            ADD_TO_WORK_DIR "gta9pxxx" "system_ext" "apex/com.android.vndk.v34.apex" 0 0 644 "u:object_r:system_file:s0"
+            ;;
         *)
             ABORT "No APEX blob available for VNDK $TARGET_BOARD_API_LEVEL"
             ;;
