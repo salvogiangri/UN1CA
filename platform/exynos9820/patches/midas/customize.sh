8505f4296859cb7e985e29a60909e2456d90d753
GoRhanHee <gorhanhee01@gmail.com>
2026-01-29 15:19:13 +0900

    platform: exynos9820: edit building error

--- platform/exynos9820/patches/midas/customize.sh
+++ platform/exynos9820/patches/midas/customize.sh
@@ -5,9 +5,9 @@ ADD_TO_WORK_DIR "p3sxxx" "vendor" "etc/midas"
 ADD_TO_WORK_DIR "p3sxxx" "vendor" "etc/VslMesDetector"
 LOG_STEP_OUT
 
-LOG "- Fixing MIDAS model detection"
-sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
-sed -i "s/p3s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+# LOG "- Fixing MIDAS model detection"
+# sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+# sed -i "s/p3s/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 
 LOG_STEP_IN "- Adding S21 (p3sxxx) Photo Remaster Service"
 DELETE_FROM_WORK_DIR "system" "system/priv-app/PhotoRemasterService/oat"
--- platform/exynos9820/patches/miscs/customize.sh
+++ platform/exynos9820/patches/miscs/customize.sh
@@ -86,35 +86,35 @@ SET_PROP "product" "bluetooth.profile.vcp.controller.enabled" "false"
 ADD_TO_WORK_DIR "b0sxxx" "system" "system/apex/com.android.btservices.apex" 0 0 644 "u:object_r:system_file:s0"
 LOG_STEP_OUT
 
-# BT-lib-patch
-if [ ! -f "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" ]; then
-    LOG_STEP_IN "- Extracting libbluetooth_jni.so from com.android.btservices.apex"
+# # BT-lib-patch
+# if [ ! -f "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" ]; then
+#     LOG_STEP_IN "- Extracting libbluetooth_jni.so from com.android.btservices.apex"
 
-    [ -d "$TMP_DIR" ] && EVAL "rm -rf \"$TMP_DIR\""
-    mkdir -p "$TMP_DIR"
+#     [ -d "$TMP_DIR" ] && EVAL "rm -rf \"$TMP_DIR\""
+#     mkdir -p "$TMP_DIR"
 
-    EVAL "unzip -j \"$WORK_DIR/system/system/apex/com.android.btservices.apex\" \"apex_payload.img\" -d \"$TMP_DIR\""
+#     EVAL "unzip -j \"$WORK_DIR/system/system/apex/com.android.btservices.apex\" \"apex_payload.img\" -d \"$TMP_DIR\""
 
-    if ! sudo -n -v &> /dev/null; then
-        LOG "\033[0;33m! Asking user for sudo password\033[0m"
-        if ! sudo -v 2> /dev/null; then
-            ABORT "Root permissions are required to unpack APEX image"
-        fi
-    fi
+#     if ! sudo -n -v &> /dev/null; then
+#         LOG "\033[0;33m! Asking user for sudo password\033[0m"
+#         if ! sudo -v 2> /dev/null; then
+#             ABORT "Root permissions are required to unpack APEX image"
+#         fi
+#     fi
 
-    mkdir -p "$TMP_DIR/tmp_out"
-    EVAL "sudo mount -o ro \"$TMP_DIR/apex_payload.img\" \"$TMP_DIR/tmp_out\""
-    EVAL "sudo cat \"$TMP_DIR/tmp_out/lib64/libbluetooth_jni.so\" > \"$WORK_DIR/system/system/lib64/libbluetooth_jni.so\""
+#     mkdir -p "$TMP_DIR/tmp_out"
+#     EVAL "sudo mount -o ro \"$TMP_DIR/apex_payload.img\" \"$TMP_DIR/tmp_out\""
+#     EVAL "sudo cat \"$TMP_DIR/tmp_out/lib64/libbluetooth_jni.so\" > \"$WORK_DIR/system/system/lib64/libbluetooth_jni.so\""
 
-    EVAL "sudo umount \"$TMP_DIR/tmp_out\""
-    rm -rf "$TMP_DIR"
+#     EVAL "sudo umount \"$TMP_DIR/tmp_out\""
+#     rm -rf "$TMP_DIR"
 
-    SET_METADATA "system" "system/lib64/libbluetooth_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+#     SET_METADATA "system" "system/lib64/libbluetooth_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
-    LOG_STEP_OUT
-fi
+#     LOG_STEP_OUT
+# fi
 
-# https://github.com/duhansysl/Bluetooth-Library-Patcher/blob/67e598ad142ed296b487a7a4585927c993d4f35d/hexpatcher.sh#L43
-HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-    "1ff828ab5e39480500352800805228ab" "1ff828ab5e392a0000142800805228ab"
+# # https://github.com/duhansysl/Bluetooth-Library-Patcher/blob/67e598ad142ed296b487a7a4585927c993d4f35d/hexpatcher.sh#L43
+# HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
+#     "1ff828ab5e39480500352800805228ab" "1ff828ab5e392a0000142800805228ab"
 
