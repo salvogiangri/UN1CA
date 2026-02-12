c087f121342bafe948f82e193a3b764e2202a23e
furbanoramos21 <107002046+furbanoramos21-testing@users.noreply.github.com>
2026-02-12 08:20:14 -0500

    a05s: init
    Co-authored-by: Tjari <zadigsm@outlook.com>
    Change-Id: I1ba7a1095299b70e361a6f7593354b2449102071
    Signed-off-by: furbanoramos21 <107002046+furbanoramos21-testing@users.noreply.github.com>

---
+++ target/a05s/sff.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2026 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy A05s (a05s)
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
