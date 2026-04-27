12ba1e2861adea500eb54587fa62bac9ddf58405
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-04-27 18:48:28 +0000

    unica: patches: saiv: introduce non-Lite BixbyVisionFramework3.5 support
    * This patch will allow targets to use BixbyVisionFramework3.5 in the full variant
      even if target doesn't support it on stock rom.
    
    * Models are extracted from Galaxy Tab S11 as S22 firmware does not offer generic TFLite
      CPU or GPU blobs.
    
    * Blobs will also be updated if target has older Android version than source.
    
    * No change is required or expected in targets.

--- unica/patches/saiv/customize.sh
+++ unica/patches/saiv/customize.sh
@@ -61,6 +61,32 @@ if [[ "$(GET_FLOATING_FEATURE_CONFIG "$FW_DIR/$TARGET_FIRMWARE_PATH/system/syste
     ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "saiv/image_understanding/db/pet_mypetsearch" 0 2000 755 "u:object_r:vendor_snap_file:s0"
 fi
 
+# SEC_PRODUCT_FEATURE_VISION_CONFIG_BIXBYVISION_VERSION
+if [[ -f "$WORK_DIR/system/system/priv-app/BixbyVisionFramework3.5/BixbyVisionFramework3.5.apk" ]]; then
+    if [[ ! -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/slens_classifier" ]] || \
+            [[ "$TARGET_PLATFORM_SDK_VERSION" -lt "$SOURCE_PLATFORM_SDK_VERSION" ]]; then
+        if [[ -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/slens_classifier" ]]; then
+            DELETE_FROM_WORK_DIR "vendor" "etc/saiv/image_understanding/db/slens_classifier"
+        fi
+        ADD_TO_WORK_DIR "gts11xx" "vendor" "etc/saiv/image_understanding/db/slens_classifier/slens_classifier_cnn.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
+        if [[ -d "$WORK_DIR/system/system/saiv/image_understanding/db/slens_classifier" ]]; then
+            DELETE_FROM_WORK_DIR "system" "system/saiv/image_understanding/db/slens_classifier"
+        fi
+        ADD_TO_WORK_DIR "gts11xx" "system" "system/saiv/image_understanding/db/slens_classifier/slens_classifier_cnn.sni" 0 0 644 "u:object_r:system_file:s0"
+    fi
+    if [[ ! -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/slens_detector" ]] || \
+            [[ "$TARGET_PLATFORM_SDK_VERSION" -lt "$SOURCE_PLATFORM_SDK_VERSION" ]]; then
+        if [[ -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/slens_detector" ]]; then
+            DELETE_FROM_WORK_DIR "vendor" "etc/saiv/image_understanding/db/slens_detector"
+        fi
+        ADD_TO_WORK_DIR "gts11xx" "vendor" "etc/saiv/image_understanding/db/slens_detector/slens_detector_cnn.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
+        if [[ -d "$WORK_DIR/system/system/saiv/image_understanding/db/slens_detector" ]]; then
+            DELETE_FROM_WORK_DIR "system" "system/saiv/image_understanding/db/slens_detector"
+        fi
+        ADD_TO_WORK_DIR "gts11xx" "system" "system/saiv/image_understanding/db/slens_detector/slens_detector_cnn.sni" 0 0 644 "u:object_r:system_file:s0"
+    fi
+fi
+
 # SEC_PRODUCT_FEATURE_CAMERA_SINGLETAKE_SOLUTIONS
 if [[ -f "$WORK_DIR/system/system/cameradata/singletake/service-feature.xml" ]]; then
     if ! grep -q "ENABLE_SINGLE_TAKE_LITE.*true" "$WORK_DIR/system/system/cameradata/singletake/service-feature.xml" 2>/dev/null; then
