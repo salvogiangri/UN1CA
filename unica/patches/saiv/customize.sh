721922b3fec1f8bf6ca333f12a0865f2967811ad
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-04-28 17:05:10 +0000

    unica: patches: saiv: support libSmartScan.camera.samsung.so blobs
    * No changes are required nor expected in targets.
    
    * This makes sure blobs for libSmartScan.camera.samsung.so are present
      and are updated automatically if target has older Android version.
    
    * Blobs are extracted from A34 5G as S22 doesn't offer generic TFLITE blobs.

--- unica/patches/saiv/customize.sh
+++ unica/patches/saiv/customize.sh
@@ -119,6 +119,17 @@ if [[ -f "$WORK_DIR/system/system/lib64/libImageCropper.camera.samsung.so" ]]; t
     fi
 fi
 
+if [[ -f "$WORK_DIR/system/system/lib64/libSmartScan.camera.samsung.so" ]]; then
+    if [[ ! -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/SS_segmenter" ]] || \
+            [[ "$TARGET_PLATFORM_SDK_VERSION" -lt "$SOURCE_PLATFORM_SDK_VERSION" ]]; then
+        if [[ -d "$WORK_DIR/vendor/etc/saiv/image_understanding/db/SS_segmenter" ]]; then
+            DELETE_FROM_WORK_DIR "vendor" "etc/saiv/image_understanding/db/SS_segmenter"
+        fi
+        ADD_TO_WORK_DIR "a34xxx" "vendor" "etc/saiv/image_understanding/db/SS_segmenter/SS_segmenter_cnn.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
+        ADD_TO_WORK_DIR "a34xxx" "vendor" "etc/saiv/image_understanding/db/SS_segmenter/SS_segmenter_cnn.info" 0 0 644 "u:object_r:vendor_configs_file:s0"
+    fi
+fi
+
 # SEC_PRODUCT_FEATURE_CAMERA_SINGLETAKE_SOLUTIONS
 if [[ -f "$WORK_DIR/system/system/cameradata/singletake/service-feature.xml" ]]; then
     if ! grep -q "ENABLE_SINGLE_TAKE_LITE.*true" "$WORK_DIR/system/system/cameradata/singletake/service-feature.xml" 2>/dev/null; then
