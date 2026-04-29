605b598d9b368580fc4352ba71a6c2988cc765a7
Majaahh <maja.blaszczak@proton.me>
2026-04-29 23:51:46 +0200

    fixup! scripts: build_incremental_ota_zip: ignore diff for identical images
    * `sha1sum` also prints path to file in "$2". In our case that would make
      the condition always false, because of different directories (source
      vs target). Print "$1" only aka the hash to fix the condition.
    
    Change-Id: If9f4e34106de3f4db9dd632d722d6c4c5423d4f1
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/internal/build_incremental_ota_zip.sh
+++ scripts/internal/build_incremental_ota_zip.sh
@@ -486,7 +486,7 @@ for p in $PARTITIONS_LIST; do
     fi
 
     if [ -f "$TMP_DIR/source/$p.img" ]; then
-        if [[ "$(sha1sum "$TMP_DIR/source/$p.img")" != "$(sha1sum "$TMP_DIR/target/$p.img")" ]]; then
+        if [[ "$(sha1sum "$TMP_DIR/source/$p.img" | cut " " -f1)" != "$(sha1sum "$TMP_DIR/target/$p.img" | cut " " -f1)" ]]; then
             _CHECK_NON_EMPTY_PARAM "TARGET_CACHE_PARTITION_SIZE" "${TARGET_CACHE_PARTITION_SIZE//none/}" || exit 1
             LOG "- Generating $p.img block diff"
             EVAL "img2sdat -o \"$TMP_DIR\" -c \"$TARGET_CACHE_PARTITION_SIZE\" --src-image \"$TMP_DIR/source/$p.img\" --src-block-map \"$TMP_DIR/source/$p.map\" --tgt-block-map \"$TMP_DIR/target/$p.map\" \"$TMP_DIR/target/$p.img\"" || exit 1
