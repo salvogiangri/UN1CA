b5c5ebb2ad43f9acd7a26d447564071169ff9965
Majaahh <maja.blaszczak@proton.me>
2026-04-30 17:17:17 +0200

    a53x: switch to V901 image tagger
    Change-Id: I390002f648eb3e0f3541ce1b0b13d66b880fc6fa
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- target/a53x/patches/camera/customize.sh
+++ target/a53x/patches/camera/customize.sh
@@ -10,7 +10,6 @@ ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libdualcam_portraitlig
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Replacing Camera blobs"
-ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_dynamic_range.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhumantracking.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/liblow_light_hdr.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
