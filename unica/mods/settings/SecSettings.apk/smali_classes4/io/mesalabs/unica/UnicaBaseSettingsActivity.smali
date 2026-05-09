artiyailbaraka/UN1CA-SM715

unica: mods: settings: abstract fragment validation

unica/mods/settings/SecSettings.apk/smali_classes4/com/android/settings/UnicaBaseSettingsActivity.smali
@@ -1,0 +1,83 @@
+.class public abstract Lcom/android/settings/UnicaBaseSettingsActivity;
+.super Lcom/android/settings/SettingsActivity;
+.source "UnicaBaseSettingsActivity.java"
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    invoke-direct {p0}, Lcom/android/settings/SettingsActivity;-><init>()V
+
+    return-void
+.end method
+
+.method private isUnicaFragment(Ljava/lang/String;)Z
+    .locals 3
+
+    if-nez p1, :cond_0
+
+    const/4 p0, 0x0
+
+    return p0
+
+    :cond_0
+    const-string v0, "io.mesalabs.unica.settings"
+
+    invoke-virtual {p1, v0}, Ljava/lang/String;->startsWith(Ljava/lang/String;)Z
+
+    move-result v0
+
+    const/16 v1, 0x2e
+
+    invoke-virtual {p1, v1}, Ljava/lang/String;->lastIndexOf(I)I
+
+    move-result v1
+
+    add-int/lit8 v1, v1, 0x1
+
+    invoke-virtual {p1, v1}, Ljava/lang/String;->substring(I)Ljava/lang/String;
+
+    move-result-object p1
+
+    if-eqz v0, :cond_1
+
+    invoke-virtual {p0}, Ljava/lang/Object;->getClass()Ljava/lang/Class;
+
+    move-result-object p0
+
+    invoke-virtual {p0}, Ljava/lang/Class;->getSimpleName()Ljava/lang/String;
+
+    move-result-object p0
+
+    new-instance v1, Ljava/lang/StringBuilder;
+
+    const-string v2, "Valid UN1CA fragment: "
+
+    invoke-direct {v1, v2}, Ljava/lang/StringBuilder;-><init>(Ljava/lang/String;)V
+
+    invoke-virtual {v1, p1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p1
+
+    invoke-virtual {p1}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+
+    move-result-object p1
+
+    invoke-static {p0, p1}, Landroid/util/Log;->d(Ljava/lang/String;Ljava/lang/String;)I
+
+    :cond_1
+    return v0
+.end method
+
+
+# virtual methods
+.method public final isValidFragment(Ljava/lang/String;)Z
+    .locals 0
+
+    invoke-direct {p0, p1}, Lcom/android/settings/UnicaBaseSettingsActivity;->isUnicaFragment(Ljava/lang/String;)Z
+
+    move-result p0
+
+    return p0
+.end method

salvogiangri/UN1CA

unica/mods/settings/SecSettings.apk/smali_classes4/io/mesalabs/unica/UnicaBaseSettingsActivity.smali
@@ -1,0 +1,83 @@
+.class public abstract Lio/mesalabs/unica/UnicaBaseSettingsActivity;
+.super Lcom/android/settings/SettingsActivity;
+.source "UnicaBaseSettingsActivity.java"
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    invoke-direct {p0}, Lcom/android/settings/SettingsActivity;-><init>()V
+
+    return-void
+.end method
+
+.method private isUnicaFragment(Ljava/lang/String;)Z
+    .locals 3
+
+    if-nez p1, :cond_0
+
+    const/4 p0, 0x0
+
+    return p0
+
+    :cond_0
+    const-string v0, "io.mesalabs.unica.settings"
+
+    invoke-virtual {p1, v0}, Ljava/lang/String;->startsWith(Ljava/lang/String;)Z
+
+    move-result v0
+
+    const/16 v1, 0x2e
+
+    invoke-virtual {p1, v1}, Ljava/lang/String;->lastIndexOf(I)I
+
+    move-result v1
+
+    add-int/lit8 v1, v1, 0x1
+
+    invoke-virtual {p1, v1}, Ljava/lang/String;->substring(I)Ljava/lang/String;
+
+    move-result-object p1
+
+    if-eqz v0, :cond_1
+
+    invoke-virtual {p0}, Ljava/lang/Object;->getClass()Ljava/lang/Class;
+
+    move-result-object p0
+
+    invoke-virtual {p0}, Ljava/lang/Class;->getSimpleName()Ljava/lang/String;
+
+    move-result-object p0
+
+    new-instance v1, Ljava/lang/StringBuilder;
+
+    const-string v2, "Valid UN1CA fragment: "
+
+    invoke-direct {v1, v2}, Ljava/lang/StringBuilder;-><init>(Ljava/lang/String;)V
+
+    invoke-virtual {v1, p1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p1
+
+    invoke-virtual {p1}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+
+    move-result-object p1
+
+    invoke-static {p0, p1}, Landroid/util/Log;->d(Ljava/lang/String;Ljava/lang/String;)I
+
+    :cond_1
+    return v0
+.end method
+
+
+# virtual methods
+.method public final isValidFragment(Ljava/lang/String;)Z
+    .locals 0
+
+    invoke-direct {p0, p1}, Lio/mesalabs/unica/UnicaBaseSettingsActivity;->isUnicaFragment(Ljava/lang/String;)Z
+
+    move-result p0
+
+    return p0
+.end method