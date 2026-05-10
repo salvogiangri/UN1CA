53e05dd17b24dfdd755894f13e49ee269914c5f5
Master37373 <102626371+Master37373@users.noreply.github.com>
2026-05-11 00:33:10 +0200

    y2s Support

---
+++ target/config.sh
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S20+ (Exynos) (y2s)
+TARGET_NAME="Galaxy S20+ (Exynos)"
+TARGET_CODENAME="y2s"
+TARGET_ASSERT_MODEL=("SM-G985F" "SM-G986B")
+TARGET_PLATFORM="exynos990"
+TARGET_FIRMWARE="SM-G986B/AUT/359847106775324"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=33
+TARGET_PRODUCT_FIRST_API_LEVEL=29
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_SUPER_PARTITION_SIZE=9437184000
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_GROUP_SIZE=9432989696
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_AUTO_BRIGHTNESS_TYPE="4"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_exynos990_xx"
+TARGET_NFC_CHIP_VENDOR="SLSI"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_ultrasonic"
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HAS_QHD_DISPLAY=true
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,48,96,120"
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_DISPLAY_CUTOUT_TYPE="center"
+TARGET_HFR_SEAMLESS_BRT="108,110"
+TARGET_HFR_SEAMLESS_LUX="200,2500"
+TARGET_IS_ESIM_SUPPORTED=true
+TARGET_HAS_HW_MDNIE=true
+TARGET_MDNIE_SUPPORTED_MODES="65303"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+TARGET_SUPPORT_WIFI_7=false
+TARGET_SUPPORT_HOTSPOT_DUALAP=false
+TARGET_SUPPORT_HOTSPOT_WPA3=false
+TARGET_SUPPORT_HOTSPOT_6GHZ=false
+TARGET_SUPPORT_HOTSPOT_WIFI_6=true
+TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN=false
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
