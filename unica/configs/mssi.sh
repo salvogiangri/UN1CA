db799da1662963d148745ecad430a9b4740f7897
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2026-05-13 20:26:17 +0200

    unica: configs: mssi: switch to M53 as extra fw
    * A34 got One UI 8.5

--- unica/configs/mssi.sh
+++ unica/configs/mssi.sh
@@ -6,6 +6,6 @@
 # Inherit source firmware configuration from essi
 source "$SRC_DIR/unica/configs/essi.sh" || return 1
 
-# Galaxy A34 5G (One UI 8.0)
-SOURCE_EXTRA_FIRMWARES=("SM-A346B/EUX/351648441234565")
+# Galaxy M53 5G (One UI 8.0)
+SOURCE_EXTRA_FIRMWARES=("SM-M536B/EUX/351069501234561")
 SOURCE_SUPER_GROUP_NAME="main"
