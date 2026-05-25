ab7fa774acc768f768b2c6dcb10bf7e748b48b5d
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-05-25 11:09:35 +0200

    Revert "unica: patches: bt-lib-patch: add MTK hex patch"
    This reverts commit d41dac57f589940aa0e3b62bb2c47c25a0062d34.

--- unica/patches/bt-lib-patch/customize.sh
+++ unica/patches/bt-lib-patch/customize.sh
@@ -28,14 +28,7 @@ if [ ! -f "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" ]; then
 fi
 
 # Disable VaultKeeper support
-# Before: [tbnz w8, #0, #0xXXXXXX]
-# After: [b #0xXXXXXX]
-if xxd -p -c 0 "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" | grep -q "2897773948050037"; then
-    HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-        "2897773948050037" "289777392a000014"
-elif xxd -p -c 0 "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" | grep -q "183a009048050037"; then
-    HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
-        "183a009048050037" "183a00902a000014"
-else
-    ABORT "No known patch available for the supplied libbluetooth_jni.so"
-fi
+# Before: [tbnz w8, #0, #0xbd260]
+# After: [b #0xbd260]
+HEX_PATCH "$WORK_DIR/system/system/lib64/libbluetooth_jni.so" \
+    "2897773948050037" "289777392a000014"
