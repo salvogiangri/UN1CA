53c1fed4515048ed3b07cb772bb87fd2ad614905
ssch71 <s01093450498@gmail.com>
2026-06-01 22:05:22 +0900

    debloat.sh 만들기

---
+++ target/gts7lwifi/debloat.sh
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2023 Salvo Giangreco
+# ...
+#
+
+# Debloat list for Galaxy Tab S7 (WIFI) (gts7wifi)
+
+# Overlays
+SYSTEM_DEBLOAT+="
+system/app/WifiRROverlayAppLls
+"
+
+# mAFPC
+SYSTEM_DEBLOAT+="
+system/bin/mafpc_write
+"
+
+# HDCP
+SYSTEM_DEBLOAT+="
+system/bin/dhkprov
+system/bin/qchdcpkprov
+system/etc/init/dhkprov.rc
+system/lib64/vendor.samsung.hardware.security.hdcp.keyprovisioning@1.0.so
+"
+
+# system_ext clean-up
+SYSTEM_EXT_DEBLOAT+="
+etc/permissions/com.qti.location.sdk.xml
+etc/permissions/com.qualcomm.location.xml
+etc/permissions/privapp-permissions-com.qualcomm.location.xml
+framework/com.qti.location.sdk.jar
+priv-app/com.qualcomm.location
+"
