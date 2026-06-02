7c47887f47190adf1f06ecf65d7ef8f49629a2fe
ssch71 <s01093450498@gmail.com>
2026-06-03 01:57:19 +0900

    gts7xlwifi/config.sh 만들기

---
+++ target/gts7xlwifi/config.sh
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A52s 5G (a52sxq)
+TARGET_NAME="Tab S7+ (WIFI)"
+TARGET_CODENAME="gts7xlwifi"
+TARGET_PLATFORM="SM8250"
+TARGET_FIRMWARE="SM-T970/XAC/R52R40DVXDJ"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_IS_TABLET=true
