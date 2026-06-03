matei9/UN1CA_SM7125

sm7125: patches: Switch to AIDL display HAL
Change-Id: Ibf810dbd4856aa734be1a56f90b442187936fff0
Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

platform/sm7125/patches/display/customize.sh
@@ -1,0 +1,70 @@
+LOG_STEP_IN "- Removing legacy display composer"
+DELETE_FROM_WORK_DIR "vendor" "bin/hw/android.hardware.graphics.composer@2.4-service"
+DELETE_FROM_WORK_DIR "vendor" "etc/init/android.hardware.graphics.composer@2.4-service.rc"
+DELETE_FROM_WORK_DIR "vendor" "etc/vintf/manifest/android.hardware.graphics.composer-qti-display.xml"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding AIDL display composer from a73xqxx"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/android.hardware.memtrack@1.0-service" 0 2000 755 "u:object_r:hal_memtrack_default_exec:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/vendor.display.color@1.0-service" 0 2000 755 "u:object_r:vendor_hal_display_color_default_exec:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/vendor.qti.hardware.display.composer-service" 0 2000 755 "u:object_r:hal_graphics_composer_default_exec:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/init/vendor.qti.hardware.display.composer-service.rc" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/snapdragon_color_libs_config.xml" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/vintf/manifest/vendor.qti.hardware.display.composer-service.xml" 0 0 644 "u:object_r:vendor_configs_file:s0"
+
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisp-aba.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplayconfig.qti.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplaydebug.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplayqos.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplayskuutils.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdpps.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdrm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdrmutils.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libgpu_tonemapper.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhdr_tm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhdrdynamic.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhdrdynamicootf.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhistogram.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqdMetaData.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqdcm-mode-parser.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqdutils.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqseed3.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqservice.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdedrm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-color.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-colormgr-algo.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-diag.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-disp-vndapis.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdmcore.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdmextension.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdmutils.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsnapdragoncolor-manager.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsnapdragoncolor-qdcm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libtinyxml2_1.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.1.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.2.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.3.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.4.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.5.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.config@2.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.postproc@1.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.composer@3.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@1.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@1.1.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@2.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@3.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@4.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapperextensions@1.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapperextensions@1.1.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.graphics.mapper@3.0-impl-qti-display.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.graphics.mapper@4.0-impl-qti-display.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.memtrack@1.0-impl.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/lights.lahaina.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/lights.qcom.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/memtrack.default.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+
+mv "$WORK_DIR/vendor/lib64/hw/lights.lahaina.so" "$WORK_DIR/vendor/lib64/hw/lights.atoll.so"
+HEX_PATCH "$WORK_DIR/vendor/lib64/hw/lights.atoll.so" "6C616861696E61" "61746F6C6C0000"
+LOG_STEP_OUT

salvogiangri/UN1CA

platform/sm7125/patches/display/customize.sh
@@ -1,0 +1,62 @@
+LOG_STEP_IN "- Removing legacy display composer"
+DELETE_FROM_WORK_DIR "vendor" "bin/hw/android.hardware.graphics.composer@2.4-service"
+DELETE_FROM_WORK_DIR "vendor" "etc/init/android.hardware.graphics.composer@2.4-service.rc"
+DELETE_FROM_WORK_DIR "vendor" "etc/vintf/manifest/android.hardware.graphics.composer-qti-display.xml"
+LOG_STEP_OUT
+
+LOG_STEP_IN "- Adding AIDL display composer from a73xqxx"
+
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/vendor.display.color@1.0-service" 0 2000 755 "u:object_r:vendor_hal_display_color_default_exec:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/vendor.qti.hardware.display.composer-service" 0 2000 755 "u:object_r:hal_graphics_composer_default_exec:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/init/vendor.qti.hardware.display.composer-service.rc" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/snapdragon_color_libs_config.xml" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "etc/vintf/manifest/vendor.qti.hardware.display.composer-service.xml" 0 0 644 "u:object_r:vendor_configs_file:s0"
+
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisp-aba.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplayconfig.qti.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplaydebug.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplayqos.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdisplayskuutils.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdpps.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdrm.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libdrmutils.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libgpu_tonemapper.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhdr_tm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhdrdynamic.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhdrdynamicootf.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libhistogram.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqdMetaData.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqdcm-mode-parser.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqdutils.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqseed3.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libqservice.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdedrm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-color.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-colormgr-algo.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-diag.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdm-disp-vndapis.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdmcore.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdmextension.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsdmutils.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsnapdragoncolor-manager.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsnapdragoncolor-qdcm.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libtinyxml2_1.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.1.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.2.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.3.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.4.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.color@1.5.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.config@2.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.display.postproc@1.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.composer@3.0.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@1.0.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@1.1.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@2.0.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@3.0.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapper@4.0.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapperextensions@1.0.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.qti.hardware.display.mapperextensions@1.1.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.graphics.mapper@3.0-impl-qti-display.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/android.hardware.graphics.mapper@4.0-impl-qti-display.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+LOG_STEP_OUT