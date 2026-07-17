a49290d18ad0e9f71cc75d4f19b09b79a81d007b
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-07-17 12:30:44 +0200

    scripts: gen_config_file: minor adjustments
    Change-Id: I25503b05e4fd4f17989aeaab06a60f6de14fa5c5

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -118,8 +118,8 @@ else
 fi
 
 # SEC_PRODUCT_FEATURE_AUDIO_SUPPORT_VIRTUAL_VIBRATION_SOUND
-if $SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION; then
-    if ! $TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION; then
+if $SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION_SOUND; then
+    if ! $TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION_SOUND; then
         APPLY_PATCH "system" "system/framework/framework.jar" \
             "$MODPATH/audio/virtual_vib/framework.jar/0001-Disable-virtual-vibration-support.patch"
         APPLY_PATCH "system" "system/framework/services.jar" \
@@ -137,9 +137,9 @@ if $SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION; then
             "$MODPATH/audio/virtual_vib/SettingsProvider.apk/0001-Disable-virtual-vibration-support.patch"
     fi
 else
-    if $TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION; then
+    if $TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION_SOUND; then
         # TODO handle this condition
-        LOG_MISSING_PATCHES "SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION" "TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION"
+        LOG_MISSING_PATCHES "SOURCE_AUDIO_SUPPORT_VIRTUAL_VIBRATION_SOUND" "TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION_SOUND"
     fi
 fi
 
