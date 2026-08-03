122114ad575ae5f4bbc187ec32d83fe635781e4e
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-08-03 09:20:50 +0200

    unica: mods: tweaks: minor adjustments
    Change-Id: I61d8c0b9335a56c8613d896b372579362ae9e834

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -2,6 +2,13 @@ if [ ! "$(GET_PROP "system" "ro.unica.version")" ]; then
     SET_PROP "system" "ro.unica.version" "$ROM_VERSION"
 fi
 
+# Show battery regulatory info in Settings
+# Requires SEM_BATTERY_PROPERTY_IC_AUTHENTICATION_RESULT support
+if [ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_BATTERY_SUPPORT_BSOH_SETTINGS")" ]; then
+    SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_BATTERY_SUPPORT_BSOH_SETTINGS" --delete
+fi
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_SETTINGS_ENABLE_EU_BATTERY_REGULATORY" "TRUE"
+
 SMALI_PATCH "system" "system/framework/framework.jar" \
     "smali/android/app/Instrumentation.smali" "replace" \
     'newApplication(Ljava/lang/Class;Landroid/content/Context;)Landroid/app/Application;' \
