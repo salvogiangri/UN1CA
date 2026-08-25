d5bf1f9a1738ab1f5986f98d7ba48c0a607bdc4c
Saad <saadelasfur@gmail.com>
2026-08-25 21:14:11 +0300

    a52sxq: installer: minor adjustments
    Change-Id: I154b489b4aaa3bc15dc1f610cd1575eae67cb472
    Co-authored-by: Majaahh <maja.blaszczak@proton.me>

--- target/a52sxq/installer/customize.sh
+++ target/a52sxq/installer/customize.sh
@@ -1,11 +1,13 @@
-LOG "- Downloading A528NKSS7GYI1_kernel.tar"
-DOWNLOAD_FILE \
-    "https://github.com/UN1CA/proprietary_vendor_samsung_sm7325/releases/download/A528NKSS7GYI1_KOO_OKR/A528NKSS7GYI1_kernel.tar" \
-    "$TMP_DIR/A528NKSS7GYI1_kernel.tar" || return 1
+REPOSITORY="https://github.com/UN1CA/proprietary_vendor_samsung_sm7325/releases/download"
+TAR_FILE="A528NKSS7GYI1_KOO_OKR/A528NKSS7GYI1_kernel.tar"
+FILE_NAME="$(basename "$TAR_FILE")"
+
+LOG "- Downloading $FILE_NAME"
+DOWNLOAD_FILE "$REPOSITORY/$TAR_FILE" "$TMP_DIR/$FILE_NAME" || return 1
 
 LOG "- Extracting dtbo.img.lz4"
-EVAL "cd \"$TMP_DIR\"; tar -xf \"A528NKSS7GYI1_kernel.tar\" \"dtbo.img.lz4\"" || return 1
-EVAL "rm -f \"$TMP_DIR/A528NKSS7GYI1_kernel.tar\"" || return 1
+EVAL "cd \"$TMP_DIR\"; tar -xf \"$FILE_NAME\" \"dtbo.img.lz4\"" || return 1
+EVAL "rm -f \"$TMP_DIR/$FILE_NAME\"" || return 1
 
 LOG "- Decompressing dtbo.img.lz4"
 EVAL "lz4 -d -f --rm \"$TMP_DIR/dtbo.img.lz4\" \"$TMP_DIR/dtbo.img\"" || return 1
@@ -16,3 +18,5 @@ if ! $TARGET_DISABLE_AVB_SIGNING; then
     LOG "- Signing dtbo.img with AVB"
     EVAL "avbtool add_hash_footer --image \"$TMP_DIR/dtbo.img\" --partition_size \"25165824\" --partition_name \"dtbo\" --hash_algorithm \"sha256\" --algorithm \"SHA256_RSA4096\" --key \"$SRC_DIR/security/avb/testkey_rsa4096.pem\"" || return 1
 fi
+
+unset REPOSITORY TAR_FILE FILE_NAME
