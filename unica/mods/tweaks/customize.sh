0ba15ef65b461f546633d4c742d78f8461ded2a4
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-08-29 00:57:34 +0200

    unica: mods: tweaks: disable FM Radio country restrictions
    Change-Id: I52a69fc14a37e57032b3e0dc248074e96e8a8e0b

--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -3,3 +3,8 @@
 LOG "- Applying \"Disable app compaction\" to /system/system/framework/services.jar"
 APPLY_PATCH "system" "system/framework/services.jar" \
     "$MODPATH/appcompactor/services.jar/0001-Disable-app-compaction.patch" &> /dev/null || true
+
+# Disable FM Radio country restrictions
+if [ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION")" ]; then
+    SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION" --delete
+fi
