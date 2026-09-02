92888e68ae1f4e3951d8a67246f4610293afd12b
Atakan <ata.kaner03@gmail.com>
2026-09-02 14:19:43 +0200

    a71: patches: dvfs: init
    Change-Id: I7659ecb7ee1281b3757e28f10795ccb849685338

---
+++ target/a71/patches/dvfs/customize.sh
@@ -0,0 +1,5 @@
+SMALI_PATCH "system" "system/priv-app/SamsungDeviceHealthManagerService/SamsungDeviceHealthManagerService.apk" \
+    "smali/r1/c.smali" "replace" \
+    '<clinit>()V' \
+    'SM6375' \
+    'SM7150'
