9372106df81d265cebb0c974bb9e6570b7b8f4ab
nhglong <its.nhglong@gmail.com>
2026-09-03 12:31:50 +0200

    sm7325: patches: blobs: Update latest gpu blobs from a73xq
    Change-Id: Ib0a50e50337d11d3fef0956cd0a1b0507485c4e4

--- platform/sm7325/patches/blobs/customize.sh
+++ platform/sm7325/patches/blobs/customize.sh
@@ -68,6 +68,33 @@ if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "34" ]; then
     ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/wpa_supplicant" 0 2000 755 "u:object_r:hal_wifi_supplicant_default_exec:s0"
 fi
 
+if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "36" ]; then
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/egl/eglSubDriverAndroid.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/egl/libEGL_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/egl/libGLESv1_CM_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/egl/libGLESv2_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/egl/libq3dtools_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/egl/libq3dtools_esx.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/hw/vulkan.adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/libCB.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/libadreno_utils.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/libgsl.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/libllvm-glnext.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib/libllvm-qcom.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/egl/eglSubDriverAndroid.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/egl/libEGL_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/egl/libGLESv1_CM_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/egl/libGLESv2_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/egl/libq3dtools_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/egl/libq3dtools_esx.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/hw/vulkan.adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libCB.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libadreno_utils.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libgsl.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libllvm-glnext.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libllvm-qcom.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+fi
+
 if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "35" ]; then
     ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/libsnaplite_native.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
 fi
