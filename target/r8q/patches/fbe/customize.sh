cb3525565ee2ad10a344fc97a47526796a9f1e5c
Atakan <ata.kaner03@gmail.com>
2026-09-09 11:49:52 +0200

    r8q: patches: fbe: add fscompress flag
    Change-Id: Icbef22999c2c9be29f9c6d07244d010d196f3155

--- target/r8q/patches/fbe/customize.sh
+++ target/r8q/patches/fbe/customize.sh
@@ -1,3 +1,3 @@
 LOG "- Patching /vendor/etc/fstab.qcom"
-EVAL "sed -i \"/[[:space:]]\/data[[:space:]]/ s|fileencryption=ice|fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc|g\" \"$WORK_DIR/vendor/etc/fstab.qcom\""
+EVAL "sed -i \"/[[:space:]]\/data[[:space:]]/ s|fileencryption=ice|fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,fscompress,keydirectory=/metadata/vold/metadata_encryption,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc|g\" \"$WORK_DIR/vendor/etc/fstab.qcom\""
 EVAL "sed -i \"/Samsung ODE/,+2d\" \"$WORK_DIR/vendor/etc/fstab.qcom\""
