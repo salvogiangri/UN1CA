accb346d097dd33b3aa20a94ef7f0747fcea083b
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-09-09 12:09:48 +0200

    scripts: firmware_utils: fix FUS access denied error
    Change-Id: I7805e86a5a46d1cb32fe386649efd032f5397ad2

--- scripts/utils/firmware_utils.sh
+++ scripts/utils/firmware_utils.sh
@@ -102,7 +102,8 @@ GET_LATEST_FIRMWARE()
     _CHECK_NON_EMPTY_PARAM "MODEL" "$1" || return 1
     _CHECK_NON_EMPTY_PARAM "CSC" "$2" || return 1
 
-    curl -s --retry 3 -m 3 "https://fota-cloud-dn.ospserver.net/firmware/$2/$1/version.xml" \
+    curl -s --retry 3 -m 3 -A "samsung $1 SyncML DM Client" \
+        "https://fota-cloud-dn.ospserver.net/firmware/$2/$1/version.xml" \
         | perl -nE 'say $1 if /<latest[^>]*>(.*?)<\/latest>/'
 }
 
