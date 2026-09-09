5b7962974154a5610073eb2489997dcf009dc787
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-09-09 11:03:46 +0200

    unica: patches: nfc: remove SKU config code
    SKU config name is not hardcode anymore and is instead fetched either via:
    - "lib-nfc-nci-" + ro.boot.product.hardware.sku + ".conf"
    - persist.nfc_cfg.config_file_name
    
    Change-Id: I27a716eb8e42c3aea43641fc144858049c89254d

--- unica/patches/nfc/customize.sh
+++ unica/patches/nfc/customize.sh
@@ -14,18 +14,6 @@ if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/libnfc-nci_temp.conf" ]
 else
     DELETE_FROM_WORK_DIR "system" "system/etc/libnfc-nci_temp.conf"
 fi
-if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/libnfc-nci-NXP_SN100U.conf" ]; then
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/libnfc-nci-NXP_SN100U.conf" 0 0 644 "u:object_r:system_file:s0"
-fi
-if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/libnfc-nci-NXP_PN553.conf" ]; then
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/libnfc-nci-NXP_PN553.conf" 0 0 644 "u:object_r:system_file:s0"
-fi
-if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/libnfc-nci-SLSI.conf" ]; then
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/libnfc-nci-SLSI.conf" 0 0 644 "u:object_r:system_file:s0"
-fi
-if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/libnfc-nci-STM_ST21.conf" ]; then
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/libnfc-nci-STM_ST21.conf" 0 0 644 "u:object_r:system_file:s0"
-fi
 
 if [ "$(GET_PROP "vendor" "ro.vendor.nfc.feature.chipname")" ]; then
     if [[ "$(GET_PROP "vendor" "ro.vendor.nfc.feature.chipname")" == "NXP_PN553" ]]; then
