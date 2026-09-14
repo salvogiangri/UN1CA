16c4e41dfa8bfb56b92e7fad2f941ee3f439a522
AstroByteX-Code <codebymianhamza@gmail.com.com>
2026-09-14 09:45:52 +0500

    target : add z3q : init

---
+++ target/z3q/patches/kernel/customize.sh
@@ -0,0 +1,14 @@
+KERNEL_REPO="https://github.com/AstroByteX-Code/kernel_samsung_z3q/releases/latest/download"
+
+LOG_STEP_IN "- Downloading Astro kernel"
+if [ -f "$WORK_DIR/kernel/boot.img" ]; then
+    rm -f "$WORK_DIR/kernel/boot.img"
+fi
+if [ -f "$WORK_DIR/kernel/dtbo.img" ]; then
+    rm -f "$WORK_DIR/kernel/dtbo.img"
+fi
+
+DOWNLOAD_FILE "$KERNEL_REPO/boot.img" "$WORK_DIR/kernel/boot.img"
+DOWNLOAD_FILE "$KERNEL_REPO/dtbo.img" "$WORK_DIR/kernel/dtbo.img"
+unset KERNEL_REPO
+LOG_STEP_OUT
