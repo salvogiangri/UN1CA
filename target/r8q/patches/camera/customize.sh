a556f5a27a0ea3d15693d3cccd24370b23c8cd7d
Atakan <ata.kaner03@gmail.com>
2026-09-16 19:31:29 +0200

    r8q: patches: camera: fix camera night mode
    Change-Id: If3b8922977dd2a471b2ee38c30034a833607b28f

--- target/r8q/patches/camera/customize.sh
+++ target/r8q/patches/camera/customize.sh
@@ -7,6 +7,7 @@ ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libTracking.polarr.so" 0 0 644 
 ADD_TO_WORK_DIR "a73xqxx" "system" "system/lib64/libYuv.polarr.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
 # Add camera libs
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libSwIsp_core.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_dynamic_range.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libhigh_res.arcsoft.so" 0 0 644 "u:object_r:system_lib_file:s0"
 EVAL "echo \"libhigh_res.arcsoft.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-arcsoft.txt\""
