cf2b3529f0fa16028662aff3d245924895f144e1
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-09-21 01:08:53 +0200

    unica: mods: tweaks: minor adjustments
    Change-Id: I07f2dd6260c72fdf826da1209895dffd62e145c9

--- unica/mods/tweaks/customize.sh
+++ unica/mods/tweaks/customize.sh
@@ -5,6 +5,11 @@ APPLY_PATCH "system" "system/framework/services.jar" \
     "$MODPATH/appcompactor/services.jar/0001-Disable-app-compaction.patch" &> /dev/null || true
 
 # Disable FM Radio country restrictions
-if [ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION")" ]; then
+if [[ "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_CHIP_VENDOR")" != "0" ]]; then
     SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FMRADIO_CONFIG_AVOID_REGION" --delete
+
+    SMALI_PATCH "system" "system/framework/samsungfmradiolib.jar" \
+        "smali/com/samsung/frameworks/fmradio/FMRadioImpl.smali" "return" \
+        'isRadioNotSupportedInRegion()Z' \
+        'false'
 fi
