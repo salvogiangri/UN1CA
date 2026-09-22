05ab0d0923a808b5c5c294626e1912a90a668f33
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-09-22 15:38:00 +0200

    sm7325: floating_feature: enable USB-PD passthrough
    Change-Id: I6e44b28b185eee596fb068fe897200dfd2799bf1

---
+++ platform/sm7325/sff.sh
@@ -0,0 +1,7 @@
+# Copyright (c) 2026 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Qualcomm Snapdragon 778G devices (sm7325)
+
+# Enable USB-PD Passthrough (depends on platform/sm7325/patches/kernel)
+SEC_FLOATING_FEATURE_BATTERY_SUPPORT_PASS_THROUGH=TRUE
